1d3631a7731112a04c51cf4843392b39a7c3b561
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2015-04-15 17:18:51 +0100

    schemas and code

--- api/io/schemas.py
+++ api/io/schemas.py
@@ -1,18 +1,19 @@
 
 import json
 from jsonschema import Draft4Validator
+from jsonschema import FormatChecker
 from jsonschema import validate
 
 
-investigationSchema = json.load(open("schemas/investigation.json"))
+investigationSchema = json.load(open("schemas/investigation_schema.json"))
 Draft4Validator.check_schema(investigationSchema)
 
-#assay_schema = json.load(open("schemas/assay_schema.json"))
+#assay_schema = json.load(open("schemas/isa_table_schema.json"))
 #Draft4Validator.check_schema(assay_schema)
 
 #assay_transcription_micro = json.load(open("schemas/assay_transcription_micro.json"))
 #Draft4Validator.check_schema(assay_transcription_micro)
 
 
-validate( json.load(open("../../json/BII-I-1_json/i_Investigation.json")), investigationSchema)
+validate(json.load(open("../../json/BII-I-1_json/i_Investigation.json")), investigationSchema, format_checker=FormatChecker())
 
