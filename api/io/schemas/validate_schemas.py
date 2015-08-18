474ed70b1995281da7ce7fc426c5ccfe98902ea9
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2015-08-18 12:22:10 +0100

    Code to validate schemas

---
+++ api/io/schemas/validate_schemas.py
@@ -0,0 +1,22 @@
+__author__ = 'agbeltran'
+
+import json, os
+from os import listdir
+from os.path import isfile, join
+from jsonschema import Draft4Validator
+
+
+def validateSchemasInFolder(folder):
+    path = os.path.abspath(folder)
+    files = [ f for f in listdir(path) if isfile(join(path,f)) ]
+
+    for schemaFile in files:
+        if (schemaFile.endswith('.json')):
+            print "Validating schema ", schemaFile, "..."
+            schema = json.load(open(join(path,schemaFile)))
+            Draft4Validator.check_schema(schema)
+            print "done."
+
+
+validateSchemasInFolder("cedar")
+validateSchemasInFolder("isa_model_version_1_0_schemas")
