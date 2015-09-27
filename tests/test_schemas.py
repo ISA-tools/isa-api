76b13391c173d57714322051dfda87e50a694c7b
David Johnson <david.johnson@oerc.ox.ac.uk>
2015-09-27 15:53:17 +0100

    Refactor schema test * Add test fixtures * General code refactor

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -1,48 +1,50 @@
 
-import json, os
+import json, os, unittest
 from jsonschema import Draft4Validator, validate, FormatChecker
 from api.io.isatab_to_json import IsatabToJsonWriter
 
-investigationSchema = json.load(open("schemas/investigation_schema.json"))
-Draft4Validator.check_schema(investigationSchema)
+class ISASchemaTest(unittest.TestCase):
+    # FIXME: What schemas exactly is this supposed to be testing? <DJ>
+    def setUp(self):
+        self._dir = os.path.join(os.path.dirname(__file__), "data")
+        self._work_dir = os.path.join(self._dir, "BII-I-1")
+        self._json_dir = self._work_dir + "-json"
 
-commonDefinitionsSchema = json.load(open("schemas/common_definitions_schema.json"))
-Draft4Validator.check_schema(commonDefinitionsSchema)
+    def tearDown(self):
+         """Remove temporary directory (generated JSON)?"""
+        #shutil.rmtree(self._json_dir, ignore_errors=True)
 
-studySchema = json.load(open("schemas/study_schema.json"))
-Draft4Validator.check_schema(studySchema)
+    def test_json_schemas(self):
+        investigation_schema = json.load(open("schemas/investigation_schema.json"))
+        Draft4Validator.check_schema(investigation_schema)
 
-assaySchema = json.load(open("schemas/assay_schema.json"))
-Draft4Validator.check_schema(assaySchema)
+        common_definitions_schema = json.load(open("schemas/common_definitions_schema.json"))
+        Draft4Validator.check_schema(common_definitions_schema)
 
-#assay_schema = json.load(open("schemas/isa_table_schema.json"))
-#Draft4Validator.check_schema(assay_schema)
+        study_schema = json.load(open("schemas/study_schema.json"))
+        Draft4Validator.check_schema(study_schema)
 
-#assay_transcription_micro = json.load(open("schemas/assay_transcription_micro.json"))
-#Draft4Validator.check_schema(assay_transcription_micro)
+        assay_schema = json.load(open("schemas/assay_schema.json"))
+        Draft4Validator.check_schema(assay_schema)
 
-#validate(json.load(open("../../json/BII-I-1_json/i_Investigation.json")), investigationSchema, format_checker=FormatChecker())
+        #assay_schema = json.load(open("schemas/isa_table_schema.json"))
+        #Draft4Validator.check_schema(assay_schema)
 
-folder_name = "BII-I-1"
-work_dir = os.path.join("../../tests/data", folder_name)
-json_dir = os.path.join("../../tests/data", folder_name + "-json")
+        #assay_transcription_micro = json.load(open("schemas/assay_transcription_micro.json"))
+        #Draft4Validator.check_schema(assay_transcription_micro)
 
-if not os.path.exists(json_dir):
-    os.makedirs(json_dir)
-# write out the json files for the isa-tab
-writer = IsatabToJsonWriter()
-writer.parsingIsatab(work_dir, json_dir)
+        #validate(json.load(open("../../json/BII-I-1_json/i_Investigation.json")), investigationSchema, format_checker=FormatChecker())
 
-validate(json.load(open(json_dir + "/i_Investigation.json")), investigationSchema, format_checker=FormatChecker())
+        if not os.path.exists(self._json_dir):
+            os.makedirs(self._json_dir)
+        # write out the json files for the isa-tab
+        writer = IsatabToJsonWriter()
+        writer.parsingIsatab(self._work_dir, self._json_dir)
 
-validate(json.load(open(json_dir + "/s_BII-S-1.json")), studySchema, format_checker=FormatChecker())
-
-validate(json.load(open(json_dir + "/s_BII-S-2.json")), studySchema, format_checker=FormatChecker())
-
-validate(json.load(open(json_dir + "/a_metabolome.json")), assaySchema, format_checker=FormatChecker())
-
-validate(json.load(open(json_dir + "/a_microarray.json")), assaySchema, format_checker=FormatChecker())
-
-validate(json.load(open(json_dir + "/a_proteome.json")), assaySchema, format_checker=FormatChecker())
-
-validate(json.load(open(json_dir + "/a_transcriptome.json")), assaySchema, format_checker=FormatChecker())
+        validate(json.load(open(self._json_dir + "/i_Investigation.json")), investigation_schema, format_checker=FormatChecker())
+        validate(json.load(open(self._json_dir + "/s_BII-S-1.json")), study_schema, format_checker=FormatChecker())
+        validate(json.load(open(self._json_dir + "/s_BII-S-2.json")), study_schema, format_checker=FormatChecker())
+        validate(json.load(open(self._json_dir + "/a_metabolome.json")), assay_schema, format_checker=FormatChecker())
+        validate(json.load(open(self._json_dir + "/a_microarray.json")), assay_schema, format_checker=FormatChecker())
+        validate(json.load(open(self._json_dir + "/a_proteome.json")), assay_schema, format_checker=FormatChecker())
+        validate(json.load(open(self._json_dir + "/a_transcriptome.json")), assay_schema, format_checker=FormatChecker())
