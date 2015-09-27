c0b0a6eb9b7b6146fd3b4fadb6bd959ca1de7504
David Johnson <david.johnson@oerc.ox.ac.uk>
2015-09-27 16:23:09 +0100

    Refactor schema test * General code refactor * Add directories to point to ISA model veresion 1.0 schemas directory

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -3,37 +3,39 @@ import json, os, unittest
 from jsonschema import Draft4Validator, validate, FormatChecker
 from api.io.isatab_to_json import IsatabToJsonWriter
 
-class ISASchemaTest(unittest.TestCase):
+class ISASchemasTest(unittest.TestCase):
     # FIXME: What schemas exactly is this supposed to be testing? <DJ>
     def setUp(self):
         self._dir = os.path.join(os.path.dirname(__file__), "data")
+        self._schema_dir = os.path.join(os.path.dirname(__file__) + "/../api/io", "schemas/isa_model_version_1_0_schemas")
         self._work_dir = os.path.join(self._dir, "BII-I-1")
         self._json_dir = self._work_dir + "-json"
 
     def tearDown(self):
-         """Remove temporary directory (generated JSON)?"""
+        """Remove temporary directory (generated JSON)?"""
         #shutil.rmtree(self._json_dir, ignore_errors=True)
+        pass
 
     def test_json_schemas(self):
-        investigation_schema = json.load(open("schemas/investigation_schema.json"))
+        investigation_schema = json.load(open(self._schema_dir + "/investigation_schema.json"))
         Draft4Validator.check_schema(investigation_schema)
 
-        common_definitions_schema = json.load(open("schemas/common_definitions_schema.json"))
-        Draft4Validator.check_schema(common_definitions_schema)
-
-        study_schema = json.load(open("schemas/study_schema.json"))
+        study_schema = json.load(open("/study_schema.json"))
         Draft4Validator.check_schema(study_schema)
 
-        assay_schema = json.load(open("schemas/assay_schema.json"))
+        assay_schema = json.load(open("/assay_schema.json"))
         Draft4Validator.check_schema(assay_schema)
 
+        #common_definitions_schema = json.load(open("/isa_model_version_1_0_schemas/common_definitions_schema.json"))
+        #Draft4Validator.check_schema(common_definitions_schema)
         #assay_schema = json.load(open("schemas/isa_table_schema.json"))
         #Draft4Validator.check_schema(assay_schema)
-
         #assay_transcription_micro = json.load(open("schemas/assay_transcription_micro.json"))
         #Draft4Validator.check_schema(assay_transcription_micro)
 
         #validate(json.load(open("../../json/BII-I-1_json/i_Investigation.json")), investigationSchema, format_checker=FormatChecker())
+
+        # What does this bit do? Is it validating schemas against test data? <DJ>
 
         if not os.path.exists(self._json_dir):
             os.makedirs(self._json_dir)
