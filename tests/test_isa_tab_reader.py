1bfc37c03f325465c170c9d14ae7f43d43473bce
David Johnson <david.johnson@oerc.ox.ac.uk>
2015-09-30 23:06:48 +0100

    Implement ISA model test * Start implementing ISA model test * Put stubs for ISA model factory tests

--- tests/test_isa_tab_reader.py
+++ tests/test_isa_tab_reader.py
@@ -1,7 +1,19 @@
 __author__ = 'dj'
 import unittest, os
+from api.io import isa_factory
+
 class TestISATabReader(unittest.TestCase):
 
     def setUp(self):
         self._dir = os.path.join(os.path.dirname(__file__), "data")
+        self._isa_tab_file = ""
+        self._isa_json = ""
+
+    def test_read_isa_tab(self):
+        isa_object = isa_factory(self._isa_tab_file)
+
+    def test_read_isa_json(self):
+        isa_object = isa_factory(self._isa_json)
+
+
 
