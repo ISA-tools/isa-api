f235f3ea6002b75eba0f3ae3c5581b6154b7357a
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2015-10-26 11:24:58 +0000

    Restructured folders, added comment schema

--- tests/test_validate_schemas.py
+++ tests/test_validate_schemas.py
@@ -5,10 +5,17 @@ from isatools.validate.validate_schemas import validateSchemasInFolder
 
 class ValidateSchemasTest(unittest.TestCase):
 
+      def test_isa_model_v1_schemas(self):
+        self.folder = "../isatools/schemas/isa_model_version_1_0_schemas"
+        validateSchemasInFolder(self.folder)
+
+      def test_isa_model_v2_schemas(self):
+        self.folder = "../isatools/schemas/isa_model_version_2_0_schemas/core"
+        validateSchemasInFolder(self.folder)
+        self.folder = "../isatools/schemas/isa_model_version_2_0_schemas/DFT"
+        validateSchemasInFolder(self.folder)
+
       def test_cedar_schemas(self):
         self.folder = "../isatools/schemas/cedar"
         validateSchemasInFolder(self.folder)
 
-      def test_cedar_schemas(self):
-        self.folder = "../isatools/schemas/isa_model_version_1_0_schemas"
-        validateSchemasInFolder(self.folder)
