63a508b024fb6cd79eade0225222e9122d26792d
David Johnson <david.johnson@oerc.ox.ac.uk>
2015-11-16 16:44:51 +0000

    Refactor tests to separate units and add assertions

--- tests/test_isa_object_model.py
+++ tests/test_isa_object_model.py
@@ -1,64 +1,58 @@
 import unittest
+
 from datetime import date
 from isatools.model.v1 import Investigation, OntologySourceReference, Publication, OntologyAnnotation, Study, \
     StudyFactor, Assay, Contact, Protocol, Process, Source, Material, MaterialAttribute, Data, Sample, Comment
-# from isatools.model.v1 import Comment, Investigation
 
 
 class ISAModelTests(unittest.TestCase):
 
-    # FIXME: Does not test Comments yet
+    def test_object_comment(self):
+        comment = Comment(
+            name="",
+            value=""
+        )
+        assert(isinstance(comment.name, str))
+        assert(isinstance(comment.value, str))
+        # TODO: Implement test on Comments in other objects
 
-    # def test_encode_isa_objects(self):
-    #     comment = Comment(
-    #         name="",
-    #         value=""
-    #     )
-    #
-    #     print(comment.to_json())
-    #
-    #     investigation = Investigation(
-    #         identifier="",
-    #         title="",
-    #         description=""
-    #     )
-    #     investigation.comments.append(comment)
-    #     print(investigation.to_json())
-
-    def test_build_isa_objects(self):
-
-        # comment = Comment(
-        #     name="",
-        #     value=""
-        # )
-
-        # ONTOLOGY SOURCE REFERENCE
+    def test_object_ontology_source_reference(self):
         ontology_source_reference = OntologySourceReference(
             name="",
             description="",
             file="",
             version=""
         )
-        # ontology_source_reference.comments.append(comment)
+        assert(isinstance(ontology_source_reference.name, str))
+        assert(isinstance(ontology_source_reference.description, str))
+        assert(isinstance(ontology_source_reference.file, str))
+        assert(isinstance(ontology_source_reference.version, str))
 
-        # ONTOLOGY ANNOTATION
+    def test_object_ontology_annotation(self):
         ontology_annotation = OntologyAnnotation(
             name="",
-            term_source=ontology_source_reference,
+            term_source=OntologySourceReference(),
             term_accession=""
         )
+        assert(isinstance(ontology_annotation.name, str))
+        assert(isinstance(ontology_annotation.term_source, OntologySourceReference))
+        assert(isinstance(ontology_annotation.term_accession, str))
 
-        # PUBLICATION
+    def test_object_publication(self):
         publication = Publication(
             pubmed_id="",
             doi="",
             author_list="",
             title="",
-            status=ontology_annotation
+            status=OntologyAnnotation()
         )
-        # publication.comments.append(comment)
+        assert(isinstance(publication.pubmed_id, str))
+        assert(isinstance(publication.doi, str))
+        assert(isinstance(publication.author_list, str))
+        assert(isinstance(publication.title, str))
+        assert(isinstance(publication.status, OntologyAnnotation))
 
-        # CONTACT
+    def test_object_contact(self):
         contact = Contact(
             last_name="",
             first_name="",
@@ -66,10 +60,15 @@ class ISAModelTests(unittest.TestCase):
             address="",
             affiliation=""
         )
-        contact.roles.append(ontology_annotation)
-        # contact.comments.append(comment)
+        contact.roles.append(OntologyAnnotation())
+        assert(isinstance(contact.first_name, str))
+        assert(isinstance(contact.mid_initials, str))
+        assert(isinstance(contact.last_name, str))
+        assert(isinstance(contact.address, str))
+        assert(isinstance(contact.affiliation, str))
+        assert(isinstance(contact.roles[0], OntologyAnnotation))
 
-        # INVESTIGATION
+    def test_object_investigation(self):
         investigation = Investigation(
             identifier="",
             title="",
@@ -77,67 +76,109 @@ class ISAModelTests(unittest.TestCase):
             submission_date=date.today(),
             public_release_date=date.today()
         )
-        investigation.ontology_source_references.append(ontology_source_reference)
-        investigation.publications.append(publication)
-        investigation.contacts.append(contact)
-        # investigation.comments.append(comment)
+        investigation.ontology_source_references.append(OntologySourceReference())
+        investigation.publications.append(Publication())
+        investigation.contacts.append(Contact())
+        investigation.studies.append(Study())
+        assert(isinstance(investigation.identifier, str))
+        assert(isinstance(investigation.title, str))
+        assert(isinstance(investigation.description, str))
+        assert(isinstance(investigation.submission_date, date))
+        assert(isinstance(investigation.public_release_date, date))
+        assert(isinstance(investigation.ontology_source_references[0], OntologySourceReference))
+        assert(isinstance(investigation.publications[0], Publication))
+        assert(isinstance(investigation.contacts[0], Contact))
+        assert(isinstance(investigation.studies[0], Study))
 
-
-        # PROTOCOL
+    def test_object_protocol(self):
         protocol = Protocol(
             name="",
-            protocol_type=ontology_annotation,
+            protocol_type=OntologyAnnotation(),
             description="",
             uri="",
             version=""
         )
-        protocol.parameters.append(ontology_annotation)
-        protocol.components.append(ontology_annotation)
-        # protocol.comments.append(comment)
+        protocol.parameters.append(OntologyAnnotation())
+        protocol.components.append(OntologyAnnotation())
+        assert(isinstance(protocol.name, str))
+        assert(isinstance(protocol.protocol_type, OntologyAnnotation))
+        assert(isinstance(protocol.description, str))
+        assert(isinstance(protocol.uri, str))
+        assert(isinstance(protocol.version, str))
 
-        # MATERIAL ATTRIBUTE
+    def test_object_material_attribute(self):
         material_attribute = MaterialAttribute(
-            ontology_annotation=ontology_annotation
+            ontology_annotation=OntologyAnnotation()
         )
+        assert(isinstance(material_attribute.ontology_annotation, OntologyAnnotation))
 
-        # SOURCE
+    def test_object_source(self):
         source = Source(
             name=""
         )
-        source.characteristics.append(material_attribute)
+        source.characteristics.append(MaterialAttribute())
+        assert(isinstance(source.name, str))
+        assert(isinstance(source.characteristics[0], MaterialAttribute))
 
-        # FACTOR
+    def test_object_study_factor(self):
         factor = StudyFactor(
             name="",
-            type_=ontology_annotation
+            type_=OntologyAnnotation()
         )
+        assert(isinstance(factor.name, str))
+        assert(isinstance(factor.type, OntologyAnnotation))
 
-        # SAMPLE
+    def test_object_sample(self):
         sample = Sample(
             name=""
         )
-        sample.characteristics.append(material_attribute)
-        sample.factors.append(factor)
+        sample.characteristics.append(MaterialAttribute())
+        sample.factors.append(StudyFactor())
+        assert(isinstance(sample.name, str))
+        assert(isinstance(sample.characteristics[0], MaterialAttribute))
+        assert(isinstance(sample.factors[0], StudyFactor))
 
-        # MATERIAL
+    def test_object_material(self):
         material = Material(name="")
-        material.characteristics.append(material_attribute)
+        material.characteristics.append(MaterialAttribute())
+        assert(isinstance(material.name, str))
+        assert(isinstance(material.characteristics[0], MaterialAttribute))
 
-        # DATA
+    def test_object_data(self):
         data = Data(name="")
+        assert(isinstance(data.name, str))
 
-        # PROCESS
+    def test_object_process(self):
         process = Process(
             name="",
-            executes_protocol=protocol,
+            executes_protocol=Protocol(),
         )
-        process.parameters.append(ontology_annotation)
-        process.inputs.append(material)
-        process.inputs.append(data)
-        process.outputs.append(material)
-        process.outputs.append(data)
+        process.parameters.append(OntologyAnnotation())
+        process.inputs.append(Material())
+        process.inputs.append(Data())
+        process.outputs.append(Material())
+        process.outputs.append(Data())
+        assert(isinstance(process.name, str))
+        assert(isinstance(process.executes_protocol, Protocol))
+        assert(isinstance(process.parameters[0], OntologyAnnotation))
+        assert(isinstance(process.inputs[0], Material))
+        assert(isinstance(process.inputs[1], Data))
+        assert(isinstance(process.outputs[0], Material))
+        assert(isinstance(process.outputs[1], Data))
 
-        #STUDY
+    def test_object_assay(self):
+        assay = Assay(
+            file_name="",
+            measurement_type=OntologyAnnotation(),
+            technology_type=OntologyAnnotation(),
+            technology_platform=""
+        )
+        assert(isinstance(assay.file_name, str))
+        assert(isinstance(assay.measurement_type, OntologyAnnotation))
+        assert(isinstance(assay.technology_type, OntologyAnnotation))
+        assert(isinstance(assay.technology_platform, str))
+
+    def test_object_study(self):
         study = Study(
             identifier="",
             title="",
@@ -146,25 +187,27 @@ class ISAModelTests(unittest.TestCase):
             public_release_date=date.today(),
             file_name=""
         )
-        study.publications.append(publication)
-        study.contacts.append(contact)
-        study.design_descriptors.append(ontology_annotation)
-        study.protocols.append(protocol)
-        study.sources.append(source)
-        study.samples.append(sample)
-        study.process_sequence.append(process)
-        assay = Assay(
-            file_name="",
-            measurement_type=ontology_annotation,
-            technology_type=ontology_annotation,
-            technology_platform=""
-        )
-        study.assays.append(assay)
-        study.samples.append(sample)
-        study.process_sequence.append(process)
-        # study.comments.append(comment)
-
-        investigation.studies.append(study)
-
-        print(investigation.to_json())
-
+        study.publications.append(Publication())
+        study.contacts.append(Contact())
+        study.design_descriptors.append(OntologyAnnotation())
+        study.protocols.append(Protocol())
+        study.sources.append(Source())
+        study.samples.append(Sample())
+        study.process_sequence.append(Process())
+        study.assays.append(Assay())
+        study.samples.append(Sample())
+        assert(isinstance(study.identifier, str))
+        assert(isinstance(study.title, str))
+        assert(isinstance(study.description, str))
+        assert(isinstance(study.submission_date, date))
+        assert(isinstance(study.public_release_date, date))
+        assert(isinstance(study.file_name, str))
+        assert(isinstance(study.publications[0], Publication))
+        assert(isinstance(study.contacts[0], Contact))
+        assert(isinstance(study.design_descriptors[0], OntologyAnnotation))
+        assert(isinstance(study.protocols[0], Protocol))
+        assert(isinstance(study.sources[0], Source))
+        assert(isinstance(study.samples[0], Sample))
+        assert(isinstance(study.process_sequence[0], Process))
+        assert(isinstance(study.assays[0], Assay))
+        assert(isinstance(study.samples[0], Sample))
