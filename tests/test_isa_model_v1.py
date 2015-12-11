adc49060447a945982e118bf6f7911e6b5760da2
David Johnson <david.johnson@oerc.ox.ac.uk>
2015-12-11 12:01:18 +0000

    Cleanup and update model tests

--- tests/test_isa_model_v1.py
+++ tests/test_isa_model_v1.py
@@ -1,165 +1,164 @@
-import unittest
-import json
-from jsonschema import RefResolver, Draft4Validator
-import os
+from unittest import TestCase
 from isatools.model.v1 import *
 
 
-class ISAObjectTests(unittest.TestCase):
+class ModelTests(TestCase):
 
     def test_object_comment(self):
         comment = Comment(
-            name="",
-            value=""
+            name='',
+            value=''
         )
-        assert(isinstance(comment.name, str))
-        assert(isinstance(comment.value, str))
+        self.assertIsInstance(comment.name, str)
+        self.assertIsInstance(comment.value, str)
         # TODO: Implement test on Comments in other objects
 
     def test_object_ontology_source_reference(self):
         ontology_source_reference = OntologySourceReference(
-            name="",
-            description="",
-            file="",
-            version=""
+            name='',
+            description='',
+            file='',
+            version=''
         )
-        assert(isinstance(ontology_source_reference.name, str))
-        assert(isinstance(ontology_source_reference.description, str))
-        assert(isinstance(ontology_source_reference.file, str))
-        assert(isinstance(ontology_source_reference.version, str))
+        self.assertIsInstance(ontology_source_reference.name, str)
+        self.assertIsInstance(ontology_source_reference.description, str)
+        self.assertIsInstance(ontology_source_reference.file, str)
+        self.assertIsInstance(ontology_source_reference.version, str)
 
     def test_object_ontology_annotation(self):
         ontology_annotation = OntologyAnnotation(
-            name="",
+            name='',
             term_source=OntologySourceReference(),
-            term_accession=""
+            term_accession=''
         )
-        assert(isinstance(ontology_annotation.name, str))
-        assert(isinstance(ontology_annotation.term_source, OntologySourceReference))
-        assert(isinstance(ontology_annotation.term_accession, str))
+        self.assertIsInstance(ontology_annotation.name, str)
+        self.assertIsInstance(ontology_annotation.term_source, OntologySourceReference)
+        self.assertIsInstance(ontology_annotation.term_accession, str)
 
     def test_object_publication(self):
         publication = Publication(
-            pubmed_id="",
-            doi="",
-            author_list="",
-            title="",
+            pubmed_id='',
+            doi='',
+            author_list='',
+            title='',
             status=OntologyAnnotation()
         )
-        assert(isinstance(publication.pubmed_id, str))
-        assert(isinstance(publication.doi, str))
-        assert(isinstance(publication.author_list, str))
-        assert(isinstance(publication.title, str))
-        assert(isinstance(publication.status, OntologyAnnotation))
+        self.assertIsInstance(publication.pubmed_id, str)
+        self.assertIsInstance(publication.doi, str)
+        self.assertIsInstance(publication.author_list, str)
+        self.assertIsInstance(publication.title, str)
+        self.assertIsInstance(publication.status, OntologyAnnotation)
 
     def test_object_contact(self):
-        contact = Contact(
-            last_name="",
-            first_name="",
-            mid_initials="",
-            address="",
-            affiliation=""
+        contact = Person(
+            last_name='',
+            first_name='',
+            mid_initials='',
+            address='',
+            affiliation=''
         )
         contact.roles.append(OntologyAnnotation())
-        assert(isinstance(contact.first_name, str))
-        assert(isinstance(contact.mid_initials, str))
-        assert(isinstance(contact.last_name, str))
-        assert(isinstance(contact.address, str))
-        assert(isinstance(contact.affiliation, str))
-        assert(isinstance(contact.roles, list))
-        assert(isinstance(contact.roles[0], OntologyAnnotation))
+        self.assertIsInstance(contact.first_name, str)
+        self.assertIsInstance(contact.mid_initials, str)
+        self.assertIsInstance(contact.last_name, str)
+        self.assertIsInstance(contact.address, str)
+        self.assertIsInstance(contact.affiliation, str)
+        self.assertIsInstance(contact.roles, list)
+        self.assertIsInstance(contact.roles[0], OntologyAnnotation)
 
     def test_object_investigation(self):
         investigation = Investigation(
-            identifier="",
-            title="",
-            description="",
+            identifier='',
+            title='',
+            description='',
             submission_date=date.today(),
             public_release_date=date.today()
         )
         investigation.ontology_source_references.append(OntologySourceReference())
         investigation.publications.append(Publication())
-        investigation.contacts.append(Contact())
+        investigation.contacts.append(Person())
         investigation.studies.append(Study())
-        assert(isinstance(investigation.identifier, str))
-        assert(isinstance(investigation.title, str))
-        assert(isinstance(investigation.description, str))
-        assert(isinstance(investigation.submission_date, date))
-        assert(isinstance(investigation.public_release_date, date))
-        assert(isinstance(investigation.ontology_source_references, list))
-        assert(isinstance(investigation.ontology_source_references[0], OntologySourceReference))
-        assert(isinstance(investigation.publications, list))
-        assert(isinstance(investigation.publications[0], Publication))
-        assert(isinstance(investigation.contacts, list))
-        assert(isinstance(investigation.contacts[0], Contact))
-        assert(isinstance(investigation.studies, list))
-        assert(isinstance(investigation.studies[0], Study))
+        self.assertIsInstance(investigation.identifier, str)
+        self.assertIsInstance(investigation.title, str)
+        self.assertIsInstance(investigation.description, str)
+        self.assertIsInstance(investigation.submission_date, date)
+        self.assertIsInstance(investigation.public_release_date, date)
+        self.assertIsInstance(investigation.ontology_source_references, list)
+        self.assertIsInstance(investigation.ontology_source_references[0], OntologySourceReference)
+        self.assertIsInstance(investigation.publications, list)
+        self.assertIsInstance(investigation.publications[0], Publication)
+        self.assertIsInstance(investigation.contacts, list)
+        self.assertIsInstance(investigation.contacts[0], Person)
+        self.assertIsInstance(investigation.studies, list)
+        self.assertIsInstance(investigation.studies[0], Study)
 
     def test_object_protocol(self):
         protocol = Protocol(
-            name="",
+            name='',
             protocol_type=OntologyAnnotation(),
-            description="",
-            uri="",
-            version=""
+            description='',
+            uri='',
+            version=''
         )
         protocol.parameters.append(OntologyAnnotation())
         protocol.components.append(OntologyAnnotation())
-        assert(isinstance(protocol.name, str))
-        assert(isinstance(protocol.protocol_type, OntologyAnnotation))
-        assert(isinstance(protocol.description, str))
-        assert(isinstance(protocol.uri, str))
-        assert(isinstance(protocol.version, str))
+        self.assertIsInstance(protocol.name, str)
+        self.assertIsInstance(protocol.protocol_type, OntologyAnnotation)
+        self.assertIsInstance(protocol.description, str)
+        self.assertIsInstance(protocol.uri, str)
+        self.assertIsInstance(protocol.version, str)
 
     def test_object_material_attribute(self):
         material_attribute = MaterialAttribute(
             characteristic=OntologyAnnotation(),
             unit=OntologyAnnotation()
         )
-        assert(isinstance(material_attribute.characteristic, OntologyAnnotation))
-        assert(isinstance(material_attribute.unit, OntologyAnnotation))
+        self.assertIsInstance(material_attribute.characteristic, OntologyAnnotation)
+        self.assertIsInstance(material_attribute.unit, OntologyAnnotation)
 
     def test_object_source(self):
         source = Source(
-            name=""
+            name=''
         )
         source.characteristics.append(MaterialAttribute())
-        assert(isinstance(source.name, str))
-        assert(isinstance(source.characteristics, list))
-        assert(isinstance(source.characteristics[0], MaterialAttribute))
+        self.assertIsInstance(source.name, str)
+        self.assertIsInstance(source.characteristics, list)
+        self.assertIsInstance(source.characteristics[0], MaterialAttribute)
 
     def test_object_study_factor(self):
         factor = StudyFactor(
-            ontology_annotation=OntologyAnnotation()
+            name='',
+            factor_type=OntologyAnnotation()
         )
-        assert(isinstance(factor.ontology_annotation, OntologyAnnotation))
+        self.assertIsInstance(factor.name, str)
+        self.assertIsInstance(factor.factor_type, OntologyAnnotation)
 
     def test_object_sample(self):
         sample = Sample(
-            name=""
+            name=''
         )
         sample.characteristics.append(MaterialAttribute())
         sample.factors.append(StudyFactor())
-        assert(isinstance(sample.name, str))
-        assert(isinstance(sample.characteristics, list))
-        assert(isinstance(sample.characteristics[0], MaterialAttribute))
-        assert(isinstance(sample.factors, list))
-        assert(isinstance(sample.factors[0], StudyFactor))
+        self.assertIsInstance(sample.name, str)
+        self.assertIsInstance(sample.characteristics, list)
+        self.assertIsInstance(sample.characteristics[0], MaterialAttribute)
+        self.assertIsInstance(sample.factors, list)
+        self.assertIsInstance(sample.factors[0], StudyFactor)
 
     def test_object_material(self):
-        material = Material(name="")
+        material = Material(name='')
         material.characteristics.append(MaterialAttribute())
-        assert(isinstance(material.name, str))
-        assert(isinstance(material.characteristics, list))
-        assert(isinstance(material.characteristics[0], MaterialAttribute))
+        self.assertIsInstance(material.name, str)
+        self.assertIsInstance(material.characteristics, list)
+        self.assertIsInstance(material.characteristics[0], MaterialAttribute)
 
     def test_object_data(self):
-        data = Data(name="")
-        assert(isinstance(data.name, str))
+        data = Data(name='')
+        self.assertIsInstance(data.name, str)
 
     def test_object_process(self):
         process = Process(
-            name="",
+            name='',
             executes_protocol=Protocol(),
         )
         process.parameters.append(OntologyAnnotation())
@@ -167,37 +166,37 @@ class ISAObjectTests(unittest.TestCase):
         process.inputs.append(Data())
         process.outputs.append(Material())
         process.outputs.append(Data())
-        assert(isinstance(process.name, str))
-        assert(isinstance(process.executes_protocol, Protocol))
-        assert(isinstance(process.parameters, list))
-        assert(isinstance(process.parameters[0], OntologyAnnotation))
-        assert(isinstance(process.inputs, list))
-        assert(isinstance(process.inputs[0], Material))
-        assert(isinstance(process.inputs[1], Data))
-        assert(isinstance(process.outputs, list))
-        assert(isinstance(process.outputs[0], Material))
-        assert(isinstance(process.outputs[1], Data))
+        self.assertIsInstance(process.name, str)
+        self.assertIsInstance(process.executes_protocol, Protocol)
+        self.assertIsInstance(process.parameters, list)
+        self.assertIsInstance(process.parameters[0], OntologyAnnotation)
+        self.assertIsInstance(process.inputs, list)
+        self.assertIsInstance(process.inputs[0], Material)
+        self.assertIsInstance(process.inputs[1], Data)
+        self.assertIsInstance(process.outputs, list)
+        self.assertIsInstance(process.outputs[0], Material)
+        self.assertIsInstance(process.outputs[1], Data)
 
     def test_object_assay(self):
         assay = Assay(
-            file_name="",
+            file_name='',
             measurement_type=OntologyAnnotation(),
             technology_type=OntologyAnnotation(),
-            technology_platform=""
+            technology_platform=''
         )
-        assert(isinstance(assay.file_name, str))
-        assert(isinstance(assay.measurement_type, OntologyAnnotation))
-        assert(isinstance(assay.technology_type, OntologyAnnotation))
-        assert(isinstance(assay.technology_platform, str))
+        self.assertIsInstance(assay.file_name, str)
+        self.assertIsInstance(assay.measurement_type, OntologyAnnotation)
+        self.assertIsInstance(assay.technology_type, OntologyAnnotation)
+        self.assertIsInstance(assay.technology_platform, str)
 
     def test_object_study(self):
         study = Study(
-            identifier="",
-            title="",
-            description="",
+            identifier='',
+            title='',
+            description='',
             submission_date=date.today(),
             public_release_date=date.today(),
-            file_name=""
+            file_name=''
         )
         study.publications.append(Publication())
         study.contacts.append(Person())
@@ -208,86 +207,27 @@ class ISAObjectTests(unittest.TestCase):
         study.process_sequence.append(Process())
         study.assays.append(Assay())
         study.samples.append(Sample())
-        assert(isinstance(study.identifier, str))
-        assert(isinstance(study.title, str))
-        assert(isinstance(study.description, str))
-        assert(isinstance(study.submission_date, date))
-        assert(isinstance(study.public_release_date, date))
-        assert(isinstance(study.file_name, str))
-        assert(isinstance(study.publications, list))
-        assert(isinstance(study.publications[0], Publication))
-        assert(isinstance(study.contacts, list))
-        assert(isinstance(study.contacts[0], Person))
-        assert(isinstance(study.design_descriptors, list))
-        assert(isinstance(study.design_descriptors[0], OntologyAnnotation))
-        assert(isinstance(study.protocols, list))
-        assert(isinstance(study.protocols[0], Protocol))
-        assert(isinstance(study.sources, list))
-        assert(isinstance(study.sources[0], Source))
-        assert(isinstance(study.samples, list))
-        assert(isinstance(study.samples[0], Sample))
-        assert(isinstance(study.process_sequence, list))
-        assert(isinstance(study.process_sequence[0], Process))
-        assert(isinstance(study.assays, list))
-        assert(isinstance(study.assays[0], Assay))
-        assert(isinstance(study.samples, list))
-        assert(isinstance(study.samples[0], Sample))
-
-
-class ISAObjectJsonWriterTest(unittest.TestCase):
-
-    """Tests to_json() implementations by validating against the schemas"""
-
-    def setUp(self):
-        self._schemas_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                                          "../isatools/schemas/isa_model_version_1_0_schemas/core/")
-
-    def _validate(self, isa_object, schema_file_name):
-        schema = json.load(open(os.path.join(self._schemas_path + schema_file_name)))
-        resolver = RefResolver(base_uri="file://" + self._schemas_path + "/", referrer=schema)
-        Draft4Validator(schema, resolver=resolver).validate(isa_object.to_json(), schema)
-
-    def test_ontology_source_reference_to_json(self):
-        self._validate(OntologySourceReference(), "ontology_source_reference_schema.json")
-
-    def test_ontology_annotation_to_json(self):
-        self._validate(OntologyAnnotation(), "ontology_annotation_schema.json")
-
-    def test_publication_to_json(self):
-        self._validate(Publication(), "publication_schema.json")
-
-    def test_contact_to_json(self):
-        self._validate(Contact(), "person_schema.json")
-
-    def test_investigation_to_json(self):
-        self._validate(Investigation(), "investigation_schema.json")
-
-    def test_protocol_to_json(self):
-        self._validate(Protocol(), "protocol_schema.json")
-
-    def test_material_to_json(self):
-        self._validate(Material(), "material_schema.json")
-
-    def test_source_to_json(self):
-        self._validate(Source(), "source_schema.json")
-
-    def test_sample_to_json(self):
-        self._validate(Sample(), "sample_schema.json")
-
-    def test_study_factor_to_json(self):
-        self._validate(StudyFactor(), "factor_schema.json")
-
-    def test_data_to_json(self):
-        self._validate(Data(), "data_schema.json")
-
-    def test_process_to_json(self):
-        self._validate(Process(), "process_schema.json")
-
-    def test_material_attribute_to_json(self):
-        self._validate(MaterialAttribute(), "material_attribute_schema.json")
-
-    def test_assay_to_json(self):
-        self._validate(Assay(), "assay_schema.json")
-
-    def test_study_to_json(self):
-        self._validate(Study(), "study_schema.json")
+        self.assertIsInstance(study.identifier, str)
+        self.assertIsInstance(study.title, str)
+        self.assertIsInstance(study.description, str)
+        self.assertIsInstance(study.submission_date, date)
+        self.assertIsInstance(study.public_release_date, date)
+        self.assertIsInstance(study.file_name, str)
+        self.assertIsInstance(study.publications, list)
+        self.assertIsInstance(study.publications[0], Publication)
+        self.assertIsInstance(study.contacts, list)
+        self.assertIsInstance(study.contacts[0], Person)
+        self.assertIsInstance(study.design_descriptors, list)
+        self.assertIsInstance(study.design_descriptors[0], OntologyAnnotation)
+        self.assertIsInstance(study.protocols, list)
+        self.assertIsInstance(study.protocols[0], Protocol)
+        self.assertIsInstance(study.sources, list)
+        self.assertIsInstance(study.sources[0], Source)
+        self.assertIsInstance(study.samples, list)
+        self.assertIsInstance(study.samples[0], Sample)
+        self.assertIsInstance(study.process_sequence, list)
+        self.assertIsInstance(study.process_sequence[0], Process)
+        self.assertIsInstance(study.assays, list)
+        self.assertIsInstance(study.assays[0], Assay)
+        self.assertIsInstance(study.samples, list)
+        self.assertIsInstance(study.samples[0], Sample)
