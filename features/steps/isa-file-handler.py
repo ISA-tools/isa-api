a3d35ef0e2b9c53a95729831e7428be0c7233d79
zigur <massimorgon@gmail.com>
2016-01-20 18:58:25 +0000

    upgraded feature: multiple tests on file retrieval

--- features/steps/isa-file-handler.py
+++ features/steps/isa-file-handler.py
@@ -1,20 +1,28 @@
-from behave import *
-from isatools.io.storage_adapter import IsaGitHubStorageAdapter
-from sure import expect
 from datetime import datetime, timezone
-import httpretty, json, hashlib
+
+import hashlib
+import httpretty
+import json
+import os
+from behave import *
+from sure import expect
+from zipfile import is_zipfile
+from urllib.parse import urljoin
+from isatools.io.storage_adapter import IsaGitHubStorageAdapter
+
 
 use_step_matcher("re")
 
-md5 = hashlib.md5()
-
 AUTH_ID = 1
-AUTH_URL = 'https://api.github.com/authorizations'
+GITHUB_API_URL = 'https://api.github.com'
 AUTH_TOKEN = "som3ar4nd0mAcc3$$tok3n"
+
+md5 = hashlib.md5()
 md5.update(AUTH_TOKEN.encode('utf-8'))
-AUTH_RES_BODY = {
+auth_url = urljoin(GITHUB_API_URL, 'authorizations')
+auth_res_body = {
     'id': AUTH_ID,
-    'url': AUTH_URL + "/" + str(AUTH_ID),
+    'url': auth_url + '/' + str(AUTH_ID),
     'app': {
         "name": "test",
         "url": "https://developer.github.com/v3/oauth_authorizations/",
@@ -59,8 +67,10 @@ def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-
-    httpretty.register_uri(httpretty.POST, AUTH_URL, body=json.dumps(AUTH_RES_BODY), content_type='application/json',
+    httpretty.register_uri(httpretty.GET, auth_url, body=json.dumps([]), content_type='application/json')
+    httpretty.register_uri(httpretty.POST, auth_url,
+                           body=json.dumps(auth_res_body),
+                           content_type='application/json',
                            status=201)
     context.isa_adapter = IsaGitHubStorageAdapter(context.username, context.password)
     expect(httpretty.has_request()).to.be.true
@@ -75,28 +85,47 @@ def step_impl(context):
     expect(context.isa_adapter.token).to.equal(AUTH_TOKEN)
 
 
-@given('a valid path in the remote repository "/path/to/source"')
+@given("an authenticated storage adapter")
+@httpretty.activate
 def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    pass
+    httpretty.register_uri(httpretty.GET, auth_url, body=json.dumps([]), content_type='application/json')
+    httpretty.register_uri(httpretty.POST, auth_url,
+                           body=json.dumps(auth_res_body),
+                           content_type='application/json',
+                           status=201)
+    context.isa_adapter = IsaGitHubStorageAdapter('uname', 'password')
+    expect(context.isa_adapter.is_authenticated).to.be.true
+    expect(context.isa_adapter.token).to.equal(AUTH_TOKEN)
 
 
-@step('an \(optional\) destination directory "/path/to/destination"')
+@step('a file object named (?P<remote_source>.+) in the remote repository')
+def step_impl(context, remote_source):
+    """
+    :type remote_source: str
+    :type context: behave.runner.Context
+    """
+    context.source_name = remote_source
+    context.source_path = urljoin(GITHUB_API_URL, '' + remote_source)
+
+
+@step('an \(optional\) destination directory (?P<destination_dir>.+) in your home folder')
+def step_impl(context, destination_dir):
+    """
+    :type destination_dir: str
+    :type context: behave.runner.Context
+    """
+    context.destination_path = os.path.join(os.path.expanduser('~'), destination_dir)
+
+
+@when("the file object is a directory")
 def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    pass
-
-
-@when("the source path points to directory")
-def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    pass
+    context.isa_adapter.download(context.source_path, context.destination_path)
 
 
 @then("it should download the whole directory it as an archived file")
@@ -104,10 +133,14 @@ def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    pass
+    file_path = os.path.join(context.destination_path, context.source_name)
+    expect(os.path.exists(file_path)).to.be.true
+    file_stat = os.stat(file_path)
+    expect(file_stat.st_size).to.be.greater_than(0)
+    expect(is_zipfile(file_path)).to.be.true
 
 
-@when("the source path points to an archive")
+@when("the file object is an archive \(i.e. a ZIP file\)")
 def step_impl(context):
     """
     :type context: behave.runner.Context
@@ -115,7 +148,7 @@ def step_impl(context):
     pass
 
 
-@then("it should save it as it is \(i\.e\. an archive\)")
+@then("it should download it as an archive \(i\.e\. a ZIP file\)")
 def step_impl(context):
     """
     :type context: behave.runner.Context
@@ -211,9 +244,27 @@ def step_impl(context):
     pass
 
 
+@when("it is a different file or a directory")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    pass
+
+
 @then("it should raise an error")
 def step_impl(context):
     """
     :type context: behave.runner.Context
     """
     pass
+
+
+@when("the source path is not correct")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    pass
+
+
