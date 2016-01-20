4d880368443e6b12904b1d4617a00ec5dae31cd1
zigur <massimorgon@gmail.com>
2016-01-20 18:59:06 +0000

    added auth retrieval before creating a new auth for user

--- isatools/io/storage_adapter.py
+++ isatools/io/storage_adapter.py
@@ -8,7 +8,7 @@ __author__ = 'massi'
 class IsaStorageAdapter(metaclass=ABCMeta):
 
     @abstractmethod
-    def download(self):
+    def download(self, source, destination):
         pass
 
     @abstractmethod
@@ -32,11 +32,17 @@ class IsaGitHubStorageAdapter(IsaStorageAdapter):
 
     AUTH_ENDPOINT = 'https://api.github.com/authorizations'
 
-    def __init__(self, username=None, password=None, note=None):
+    def __init__(self, username=None, password=None, note=None, scopes=('gist', 'repo')):
+        """
+        Initialize an ISA Storage Adapter to perform CRUD operations on a remote GitHub repository
+        :type username: str
+        :type password: str
+        :type note str
+        """
         self._authorization = {}
         if username and password:
             payload = {
-                "scopes": ["gist", "repo"],
+                "scopes": list(scopes),
                 "note": note or "Authorization to access the ISA data sets"
             }
             headers = {
@@ -44,9 +50,24 @@ class IsaGitHubStorageAdapter(IsaStorageAdapter):
                 "accept": "application/json"
 
             }
-            res = requests.post(self.AUTH_ENDPOINT,  json=payload, headers=headers, auth=(username, password))
-            if res.status_code == requests.codes.created:
-                self._authorization = json.loads(res.text or res.content)
+            # retrieve all the existing authorizations for user
+            res = requests.get(self.AUTH_ENDPOINT, headers= headers, auth=(username, password))
+            if res.status_code == requests.codes.ok:
+                auths = json.loads(res.text)
+
+                # filter the existing authorizations according to thw provided criteria (note and scopes)
+                auths = [auth for auth in auths
+                         if auth['note'] == payload['note'] and auth['scopes'] == payload['scopes']]
+
+                # if the required authorization already exists use it
+                if len(auths) > 0:
+                    self._authorization = auths[0]
+
+                # otherwise require a new authorization
+                else:
+                    res = requests.post(self.AUTH_ENDPOINT,  json=payload, headers=headers, auth=(username, password))
+                    if res.status_code == requests.codes.created:
+                        self._authorization = json.loads(res.text or res.content)
 
     @property
     def token(self):
@@ -59,7 +80,12 @@ class IsaGitHubStorageAdapter(IsaStorageAdapter):
         else:
             return False
 
-    def download(self):
+    def download(self, source, destination):
+        """
+        Call to download a resource from a remote GitHub repository
+        :type source: str
+        :type destination str
+        """
         pass
 
     def retrieve(self):
