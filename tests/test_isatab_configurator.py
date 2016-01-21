5450bb7b5743dec8e49b8201d777c3836a33603d
David Johnson <david.johnson@oerc.ox.ac.uk>
2016-01-21 15:03:32 +0000

    Refactor config object accessors to be less verbose

--- tests/test_isatab_configurator.py
+++ tests/test_isatab_configurator.py
@@ -15,32 +15,32 @@ class ISATabConfiguratorTest(TestCase):
     def test_parse(self):
         from isatools.io import isatab_configurator as configurator
         config_obj = configurator.parse(os.path.join(self._config_dir, 'genome_seq.xml'), True)  # Silent output
-        self.assertEqual(len(config_obj.get_isatab_configuration()), 1)
-        self.assertEqual(config_obj.get_isatab_configuration()[0].get_table_name(), 'genome_seq')
-        self.assertEqual(config_obj.get_isatab_configuration()[0].get_isatab_assay_type(), 'generic_assay')
-        self.assertEqual(config_obj.get_isatab_configuration()[0].get_isatab_conversion_target(), 'sra')
-        self.assertEqual(config_obj.get_isatab_configuration()[0].get_measurement().get_term_label(),
+        self.assertEqual(len(config_obj.isatab_configuration), 1)
+        self.assertEqual(config_obj.isatab_configuration[0].table_name, 'genome_seq')
+        self.assertEqual(config_obj.isatab_configuration[0].isatab_assay_type, 'generic_assay')
+        self.assertEqual(config_obj.isatab_configuration[0].isatab_conversion_target, 'sra')
+        self.assertEqual(config_obj.isatab_configuration[0].measurement.term_label,
                          'genome sequencing')
-        self.assertEqual(config_obj.get_isatab_configuration()[0].get_technology().get_term_label(),
+        self.assertEqual(config_obj.isatab_configuration[0].technology.term_label,
                          'nucleotide sequencing')
-        self.assertEqual(len(config_obj.get_isatab_configuration()[0].get_field()), 15)
-        self.assertEqual(config_obj.get_isatab_configuration()[0].get_field()[0].get_header(), 'Sample Name')
-        self.assertEqual(config_obj.get_isatab_configuration()[0].get_field()[0].get_data_type(), 'String')
-        self.assertFalse(config_obj.get_isatab_configuration()[0].get_field()[0].get_is_file_field())
-        self.assertFalse(config_obj.get_isatab_configuration()[0].get_field()[0].get_is_multiple_value())
-        self.assertTrue(config_obj.get_isatab_configuration()[0].get_field()[0].get_is_required())
-        self.assertFalse(config_obj.get_isatab_configuration()[0].get_field()[0].get_is_hidden())
-        self.assertEqual(config_obj.get_isatab_configuration()[0].get_field()[0].get_description(), 'sample name')
-        self.assertEqual(config_obj.get_isatab_configuration()[0].get_field()[0].get_default_value(), '')
-        self.assertEqual(config_obj.get_isatab_configuration()[0].get_field()[0].get_generated_value_template(),
+        self.assertEqual(len(config_obj.isatab_configuration[0].field), 15)
+        self.assertEqual(config_obj.isatab_configuration[0].field[0].header, 'Sample Name')
+        self.assertEqual(config_obj.isatab_configuration[0].field[0].data_type, 'String')
+        self.assertFalse(config_obj.isatab_configuration[0].field[0].is_file_field)
+        self.assertFalse(config_obj.isatab_configuration[0].field[0].is_multiple_value)
+        self.assertTrue(config_obj.isatab_configuration[0].field[0].is_required)
+        self.assertFalse(config_obj.isatab_configuration[0].field[0].is_hidden)
+        self.assertEqual(config_obj.isatab_configuration[0].field[0].description, 'sample name')
+        self.assertEqual(config_obj.isatab_configuration[0].field[0].default_value, '')
+        self.assertEqual(config_obj.isatab_configuration[0].field[0].generated_value_template,
                          '[INSTITUTION].Group-[GROUP_NO].Subject-[SUBJECT_NO].[SAMPLE_EXTRACT]')
-        self.assertEqual(len(config_obj.get_isatab_configuration()[0].get_protocol_field()), 4)
-        self.assertEqual(config_obj.get_isatab_configuration()[0].get_protocol_field()[0].get_protocol_type(),
+        self.assertEqual(len(config_obj.isatab_configuration[0].protocol_field), 4)
+        self.assertEqual(config_obj.isatab_configuration[0].protocol_field[0].protocol_type,
                          'nucleic acid extraction')
 
     def test_load_configs(self):
         from isatools.io import isatab_configurator as configurator
         config_dict = configurator.load(self._config_dir)
         self.assertEqual(len(config_dict), 30)
-        self.assertEqual(config_dict[('metagenome sequencing', 'nucleotide sequencing')].get_isatab_configuration()[0]
-                         .get_table_name(),'metagenome_seq')
+        self.assertEqual(config_dict[('metagenome sequencing', 'nucleotide sequencing')].isatab_configuration[0]
+                         .table_name,'metagenome_seq')
