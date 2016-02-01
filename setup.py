0292f247cb04ba442889365c0cbc8ace1277b759
zigur <massimorgon@gmail.com>
2016-02-01 16:31:37 +0000

    added test suite

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='isatools',
@@ -14,4 +14,5 @@ setup(
         'Programming Language :: Python :: 3.4',
         ],
     url='http://www.isa-tools.org/',
+    test_suite='tests'
 )
