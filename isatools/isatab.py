842c05b7c39828d5d2cb0790452f024a1d0af295
David Johnson <david.johnson@oerc.ox.ac.uk>
2016-02-18 21:47:05 +0000

    Implement tab equality assert function - currently works with study files; towards #83 #10

--- isatools/isatab.py
+++ isatools/isatab.py
@@ -1,7 +1,6 @@
-from symbol import lambdef
+from _hashlib import new
 from .model.v1 import *
-from isatools.io import isatab_parser, isatab_configurator
-from isatools.io.isatab_configurator import FieldType, ProtocolFieldType, StructuredFieldType
+from isatools.io import isatab_parser
 import os
 import sys
 import pandas as pd
@@ -1229,6 +1228,90 @@ def write_study_table_files(inv_obj, output_dir):
             df.to_csv(path_or_buf=open(os.path.join(output_dir, study_obj.filename), 'w'), index=False, sep='\t', encoding='utf-8',)
     else:
         raise IOError("Input object is not a valid Investigation object")
+
+
+def assertTabEqual(fp_x, fp_y):
+    def diff(a, b):
+        b = set(b)
+        return [aa for aa in a if aa not in b]
+    """
+    Test for equality of tab files, only down to level of content - should not be taken as canonical equality, but
+    rather that all the expected content matches to both input files
+    :param fp_x: File descriptor of a ISAtab file
+    :param fp_y: File descriptor of another  ISAtab file
+    :return: True or False plus any AssertionErrors
+    """
+    import numpy as np
+    df_x = pd.read_csv(fp_x, sep='\t', encoding='utf-8')
+    df_y = pd.read_csv(fp_y, sep='\t', encoding='utf-8')
+    try:
+        # drop empty columns
+        df_x = df_x.replace('', np.nan)
+        df_x = df_x.dropna(axis=1, how='all')
+        df_y = df_y.replace('', np.nan)
+        df_y = df_y.dropna(axis=1, how='all')
+
+        is_cols_equal = set([x.split('.', 1)[0] for x in df_x.columns]) == set([x.split('.', 1)[0] for x in df_y.columns])
+        if not is_cols_equal:
+            print('x: ' + df_x.columns)
+            print('y: ' + df_y.columns)
+            print(diff(df_x.columns, df_y.columns))
+            raise AssertionError("Columns in x do not match those in y")
+
+        # reindex to add contexts for duplicate named columns (i.e. Term Accession Number, Unit, etc.)
+        import re
+        char_regex = re.compile('Characteristics\[(.*?)\]')
+        pv_regex = re.compile('Parameter Value\[(.*?)\]')
+        fv_regex = re.compile('Factor Value\[(.*?)\]')
+        newcolsx = list()
+        for col in df_x.columns:
+            newcolsx.append(col)
+        for i, col in enumerate(df_x.columns):
+            if char_regex.match(col) or pv_regex.match(col) or fv_regex.match(col):
+                try:
+                    if 'Unit' in df_x.columns[i+1]:
+                        newcolsx[i+1] = col + '/Unit'
+                        if 'Term Source REF' in df_x.columns[i+2]:
+                            newcolsx[i+2] = col + '/Unit/Term Source REF'
+                        if 'Term Accession Number' in df_x.columns[i+3]:
+                            newcolsx[i+3] = col + '/Unit/Term Accession Number'
+                    elif 'Term Source REF' in df_x.columns[i+1]:
+                        newcolsx[i+1] = col + '/Term Source REF'
+                        if 'Term Accession Number' in df_x.columns[i+2]:
+                            newcolsx[i+2] = col + '/Term Accession Number'
+                except IndexError:
+                    pass
+        df_x.columns = newcolsx
+        newcolsy = list()
+        for col in df_y.columns:
+            newcolsy.append(col)
+        for i, col in enumerate(df_y.columns):
+            if char_regex.match(col) or pv_regex.match(col) or fv_regex.match(col):
+                try:
+                    if 'Unit' in df_y.columns[i+1]:
+                        newcolsy[i+1] = col + '/Unit'
+                        if 'Term Source REF' in df_y.columns[i+2]:
+                            newcolsy[i+2] = col + '/Unit/Term Source REF'
+                        if 'Term Accession Number' in df_y.columns[i+3]:
+                            newcolsy[i+3] = col + '/Unit/Term Accession Number'
+                    elif 'Term Source REF' in df_y.columns[i+1]:
+                        newcolsy[i+1] = col + '/Term Source REF'
+                        if 'Term Accession Number' in df_y.columns[i+2]:
+                            newcolsy[i+2] = col + '/Term Accession Number'
+                except IndexError:
+                    pass
+        df_y.columns = newcolsy
+        for colx in df_x.columns:
+            for eachx, eachy in zip(df_x[colx], df_y[colx]):
+                if eachx != eachy:
+                    print(df_x[colx])
+                    print(df_y[colx])
+                    raise AssertionError("Value: " + str(eachx) + ", does not match: " + str(eachy))
+        print("Well, you got here so the files must be same-ish... well done, you!")
+        return True
+    except AssertionError as e:
+        print(str(e))
+        return False
 
 
 def read_investigation_file(fp):
