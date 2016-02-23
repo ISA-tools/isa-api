5cac10f4db5ddde5dfef8291e0594c6f04f06531
David Johnson <david.johnson@oerc.ox.ac.uk>
2016-02-23 14:58:31 +0000

    Reset tests to working ones

--- tests/test_json2isatab.py
+++ tests/test_json2isatab.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 import os
 import shutil
 from isatools.convert import json2isatab
-from isatools.isatab import assert_tab_equal
+from isatools.isatab import assert_tab_content_equal
 
 
 class JsonToTabTest(TestCase):
@@ -15,44 +15,53 @@ class JsonToTabTest(TestCase):
 
     def tearDown(self):
         shutil.rmtree(self._tmp, ignore_errors=True)
-        pass
 
     def test_source_split_investigation(self):
         json2isatab.convert(open(os.path.join(self._dir, 'TEST-ISA-source-split.json')), self._tmp)
-        self.assertTrue(assert_tab_equal(open(os.path.join(self._tmp, 'i_Investigation.txt')),
-                                         open(os.path.join(self._dir, 'data/TEST-ISA-source-split/i_Investigation.txt'))))
+        self.assertTrue(assert_tab_content_equal(open(os.path.join(self._tmp, 'i_investigation.txt')),
+                                                 open(os.path.join(self._dir, 'data/TEST-ISA-source-split/i_Investigation.txt'))))
 
     def test_source_split_study_table(self):
         json2isatab.convert(open(os.path.join(self._dir, 'TEST-ISA-source-split.json')), self._tmp)
-        self.assertTrue(assert_tab_equal(open(os.path.join(self._tmp, 's_TEST-Template1-Splitting.txt')),
-                                         open(os.path.join(self._dir, 'data/TEST-ISA-source-split/s_TEST-Template1-Splitting.txt'))))
+        self.assertTrue(assert_tab_content_equal(open(os.path.join(self._tmp, 's_TEST-Template1-Splitting.txt')),
+                                                 open(os.path.join(self._dir, 'data/TEST-ISA-source-split/s_TEST-Template1-Splitting.txt'))))
 
     def test_source_split_assay_table(self):
         json2isatab.convert(open(os.path.join(self._dir, 'TEST-ISA-source-split.json')), self._tmp)
-        self.assertTrue(assert_tab_equal(open(os.path.join(self._tmp, 'a_test-template1-splitting_transcription_profiling_DNA_microarray.txt')),
-                                         open(os.path.join(self._dir, 'data/TEST-ISA-source-split/a_test-template1-splitting_transcription_profiling_DNA_microarray.txt'))))
+        self.assertTrue(assert_tab_content_equal(open(os.path.join(self._tmp, 'a_test-template1-splitting_transcription_profiling_DNA_microarray.txt')),
+                                                 open(os.path.join(self._dir, 'data/TEST-ISA-source-split/a_test-template1-splitting_transcription_profiling_DNA_microarray.txt'))))
+
+    def test_sample_pool_investigation(self):
+        json2isatab.convert(open(os.path.join(self._dir, 'TEST-ISA-sample-pool.json')), self._tmp)
+        self.assertTrue(assert_tab_content_equal(open(os.path.join(self._tmp, 'i_investigation.txt')),
+                                                 open(os.path.join(self._dir, 'data/TEST-ISA-sample-pool/i_Investigation.txt'))))
 
     def test_sample_pool_study_table(self):
         json2isatab.convert(open(os.path.join(self._dir, 'TEST-ISA-sample-pool.json')), self._tmp)
-        self.assertTrue(assert_tab_equal(open(os.path.join(self._tmp, 's_TEST-Template3-Splitting.txt')),
-                                         open(os.path.join(self._dir, 'data/TEST-ISA-sample-pool/s_TEST-Template3-Splitting.txt'))))
+        self.assertTrue(assert_tab_content_equal(open(os.path.join(self._tmp, 's_TEST-Template3-Splitting.txt')),
+                                                 open(os.path.join(self._dir, 'data/TEST-ISA-sample-pool/s_TEST-Template3-Splitting.txt'))))
 
     def test_sample_pool_assay_table(self):
         json2isatab.convert(open(os.path.join(self._dir, 'TEST-ISA-sample-pool.json')), self._tmp)
-        self.assertTrue(assert_tab_equal(open(os.path.join(self._tmp, 'a_test-template3-splitting_transcription_profiling_DNA_microarray.txt')),
-                                         open(os.path.join(self._dir, 'data/TEST-ISA-sample-pool/a_test-template3-splitting_transcription_profiling_DNA_microarray.txt'))))
+        self.assertTrue(assert_tab_content_equal(open(os.path.join(self._tmp, 'a_test-template3-splitting_transcription_profiling_DNA_microarray.txt')),
+                                                 open(os.path.join(self._dir, 'data/TEST-ISA-sample-pool/a_test-template3-splitting_transcription_profiling_DNA_microarray.txt'))))
+
+    def test_bii_s_3_investigation(self):
+        json2isatab.convert(open(os.path.join(self._dir, 'data/BII-S-3/BII-S-3.json')), self._tmp)
+        self.assertTrue(assert_tab_content_equal(open(os.path.join(self._tmp, 'i_investigation.txt')),
+                                         open(os.path.join(self._dir, 'data/BII-S-3/i_gilbert.txt'))))
 
     def test_bii_s_3_study_table(self):
         json2isatab.convert(open(os.path.join(self._dir, 'data/BII-S-3/BII-S-3.json')), self._tmp)
-        self.assertTrue(assert_tab_equal(open(os.path.join(self._tmp, 's_BII-S-3.txt')),
+        self.assertTrue(assert_tab_content_equal(open(os.path.join(self._tmp, 's_BII-S-3.txt')),
                                          open(os.path.join(self._dir, 'data/BII-S-3/s_BII-S-3.txt'))))
 
     def test_bii_s_3_assay_table_Tx(self):
         json2isatab.convert(open(os.path.join(self._dir, 'data/BII-S-3/BII-S-3.json')), self._tmp)
-        self.assertTrue(assert_tab_equal(open(os.path.join(self._tmp, 'a_gilbert-assay-Tx.txt')),
+        self.assertTrue(assert_tab_content_equal(open(os.path.join(self._tmp, 'a_gilbert-assay-Tx.txt')),
                                          open(os.path.join(self._dir, 'data/BII-S-3/a_gilbert-assay-Tx.txt'))))
 
     def test_bii_s_3_assay_table_Gx(self):
         json2isatab.convert(open(os.path.join(self._dir, 'data/BII-S-3/BII-S-3.json')), self._tmp)
-        self.assertTrue(assert_tab_equal(open(os.path.join(self._tmp, 'a_gilbert-assay-Gx.txt')),
+        self.assertTrue(assert_tab_content_equal(open(os.path.join(self._tmp, 'a_gilbert-assay-Gx.txt')),
                                          open(os.path.join(self._dir, 'data/BII-S-3/a_gilbert-assay-Gx.txt'))))
--- tests/test_two_way_convert.py
+++ tests/test_two_way_convert.py
@@ -20,92 +20,86 @@ class TwoWayConvertTest(TestCase):
         source_dir = os.path.join(self._dir, 'data/TEST-ISA-sample-pool/')
         isatab2json.convert(source_dir, self._tmp)
         json2isatab.convert(open(os.path.join(self._tmp, 'TEST-ISA-sample-pool.json')), self._tmp)
-        self.assertTrue(isatab.assert_tab_equal(open(os.path.join(source_dir, 'i_investigation.txt')),
+        self.assertTrue(isatab.assert_tab_content_equal(open(os.path.join(source_dir, 'i_investigation.txt')),
                                                 open(os.path.join(self._tmp, 'i_investigation.txt'))))
-        self.assertTrue(isatab.assert_tab_equal(open(os.path.join(source_dir, 's_TEST-Template3-Splitting.txt')),
+        self.assertTrue(isatab.assert_tab_content_equal(open(os.path.join(source_dir, 's_TEST-Template3-Splitting.txt')),
                                                 open(os.path.join(self._tmp, 's_TEST-Template3-Splitting.txt'))))
-        self.assertTrue(isatab.assert_tab_equal(open(os.path.join(source_dir, 'a_test-template3-splitting_transcription_profiling_DNA_microarray.txt')),
+        self.assertTrue(isatab.assert_tab_content_equal(open(os.path.join(source_dir, 'a_test-template3-splitting_transcription_profiling_DNA_microarray.txt')),
                                                 open(os.path.join(self._tmp, 'a_test-template3-splitting_transcription_profiling_DNA_microarray.txt'))))
 
     def test_isa_source_split(self):
         source_dir = os.path.join(self._dir, 'data/TEST-ISA-source-split/')
         isatab2json.convert(source_dir, self._tmp)
         json2isatab.convert(open(os.path.join(self._tmp, 'TEST-ISA-source-split.json')), self._tmp)
-        self.assertTrue(isatab.assert_tab_equal(open(os.path.join(source_dir, 'i_investigation.txt')),
+        self.assertTrue(isatab.assert_tab_content_equal(open(os.path.join(source_dir, 'i_investigation.txt')),
                                                 open(os.path.join(self._tmp, 'i_investigation.txt'))))
-        self.assertTrue(isatab.assert_tab_equal(open(os.path.join(source_dir, 's_TEST-Template1-Splitting.txt')),
+        self.assertTrue(isatab.assert_tab_content_equal(open(os.path.join(source_dir, 's_TEST-Template1-Splitting.txt')),
                                                 open(os.path.join(self._tmp, 's_TEST-Template1-Splitting.txt'))))
-        self.assertTrue(isatab.assert_tab_equal(open(os.path.join(source_dir, 'a_test-template1-splitting_transcription_profiling_DNA_microarray.txt')),
+        self.assertTrue(isatab.assert_tab_content_equal(open(os.path.join(source_dir, 'a_test-template1-splitting_transcription_profiling_DNA_microarray.txt')),
                                                 open(os.path.join(self._tmp, 'a_test-template1-splitting_transcription_profiling_DNA_microarray.txt'))))
 
     # def test_isa_repeated_measure(self):
     #     source_dir = os.path.join(self._dir, 'data/TEST-ISA-repeated-measure/')
     #     isatab2json.convert(source_dir, self._tmp)
     #     json2isatab.convert(open(os.path.join(self._tmp, 'TEST-ISA-repeated-measure.json')), self._tmp)
-    #     isatab.assert_tab_equal(open(os.path.join(source_dir, 'i_investigation.txt')),
+    #     isatab.assert_tab_content_equal(open(os.path.join(source_dir, 'i_investigation.txt')),
     #                             open(os.path.join(self._tmp, 'i_investigation.txt')))
-    #     isatab.assert_tab_equal(open(os.path.join(source_dir, 's_TEST-Template5-repeatedmeasure.txt')),
+    #     isatab.assert_tab_content_equal(open(os.path.join(source_dir, 's_TEST-Template5-repeatedmeasure.txt')),
     #                             open(os.path.join(self._tmp, 's_TEST-Template5-repeatedmeasure.txt')))
-    #     isatab.assert_tab_equal(open(os.path.join(source_dir, 'a_test-template5-repeatedmeasure_transcription_profiling_DNA_microarray.txt')),
+    #     isatab.assert_tab_content_equal(open(os.path.join(source_dir, 'a_test-template5-repeatedmeasure_transcription_profiling_DNA_microarray.txt')),
     #                             open(os.path.join(self._tmp, 'a_test-template5-repeatedmeasure_transcription_profiling_DNA_microarray.txt')))
+    """ Repeated measure test produces too many study table rows """
+
 
     # def test_isa_charac_param_factor(self):
     #     source_dir = os.path.join(self._dir, 'data/TEST-ISA-charac-param-factor/')
     #     isatab2json.convert(source_dir, self._tmp)
     #     json2isatab.convert(open(os.path.join(self._tmp, 'TEST-ISA-charac-param-factor.json')), self._tmp)
-    #     isatab.assert_tab_equal(open(os.path.join(source_dir, 'i_investigation.txt')),
+    #     isatab.assert_tab_content_equal(open(os.path.join(source_dir, 'i_investigation.txt')),
     #                             open(os.path.join(self._tmp, 'i_investigation.txt')))
-    #     isatab.assert_tab_equal(open(os.path.join(source_dir, 's_TEST-Template1-Splitting.txt')),
+    #     isatab.assert_tab_content_equal(open(os.path.join(source_dir, 's_TEST-Template1-Splitting.txt')),
     #                             open(os.path.join(self._tmp, 's_TEST-Template1-Splitting.txt')))
-    #     isatab.assert_tab_equal(open(os.path.join(source_dir, 'a_test-template1-splitting_transcription_profiling_DNA_microarray.txt')),
+    #     isatab.assert_tab_content_equal(open(os.path.join(source_dir, 'a_test-template1-splitting_transcription_profiling_DNA_microarray.txt')),
     #                             open(os.path.join(self._tmp, 'a_test-template1-splitting_transcription_profiling_DNA_microarray.txt')))
-    """
-    problem with intermediate json
-    """
+    """Problem with intermediate json"""
 
     # def test_isa_bii_i_1(self):
     #     source_dir = os.path.join(self._dir, 'data/BII-I-1/')
     #     isatab2json.convert(source_dir, self._tmp)
     #     json2isatab.convert(open(os.path.join(self._tmp, 'BII-I-1.json')), self._tmp)
-    #     isatab.assert_tab_equal(open(os.path.join(source_dir, 'i_investigation.txt')),
+    #     isatab.assert_tab_content_equal(open(os.path.join(source_dir, 'i_investigation.txt')),
     #                             open(os.path.join(self._tmp, 'i_investigation.txt')))
-    #     isatab.assert_tab_equal(open(os.path.join(source_dir, 's_BII-S-1.txt')),
+    #     isatab.assert_tab_content_equal(open(os.path.join(source_dir, 's_BII-S-1.txt')),
     #                             open(os.path.join(self._tmp, 's_BII-S-2.txt')))
-    #     isatab.assert_tab_equal(open(os.path.join(source_dir, 'a_metabolome.txt')),
+    #     isatab.assert_tab_content_equal(open(os.path.join(source_dir, 'a_metabolome.txt')),
     #                             open(os.path.join(self._tmp, 'a_metabolome.txt')))
-    #     isatab.assert_tab_equal(open(os.path.join(source_dir, 'a_microarray.txt')),
+    #     isatab.assert_tab_content_equal(open(os.path.join(source_dir, 'a_microarray.txt')),
     #                             open(os.path.join(self._tmp, 'a_microarray.txt')))
-    #     isatab.assert_tab_equal(open(os.path.join(source_dir, 'a_transcriptome.txt')),
+    #     isatab.assert_tab_content_equal(open(os.path.join(source_dir, 'a_transcriptome.txt')),
     #                             open(os.path.join(self._tmp, 'a_transcriptome.txt')))
-    """
-    problem with intermediate json
-    """
+    """Problem with intermediate json"""
 
     def test_isa_bii_s_3(self):
         source_dir = os.path.join(self._dir, 'data/BII-S-3/')
         isatab2json.convert(source_dir, self._tmp)
         json2isatab.convert(open(os.path.join(self._tmp, 'BII-S-3.json')), self._tmp)
-        self.assertTrue(isatab.assert_tab_equal(open(os.path.join(source_dir, 'i_gilbert.txt')),
+        self.assertTrue(isatab.assert_tab_content_equal(open(os.path.join(source_dir, 'i_gilbert.txt')),
                                                 open(os.path.join(self._tmp, 'i_investigation.txt'))))
-        self.assertTrue(isatab.assert_tab_equal(open(os.path.join(source_dir, 's_BII-S-3.txt')),
+        self.assertTrue(isatab.assert_tab_content_equal(open(os.path.join(source_dir, 's_BII-S-3.txt')),
                                                 open(os.path.join(self._tmp, 's_BII-S-3.txt'))))
-        self.assertTrue(isatab.assert_tab_equal(open(os.path.join(source_dir, 'a_gilbert-assay-Gx.txt')),
+        self.assertTrue(isatab.assert_tab_content_equal(open(os.path.join(source_dir, 'a_gilbert-assay-Gx.txt')),
                                                 open(os.path.join(self._tmp, 'a_gilbert-assay-Gx.txt'))))
-        self.assertTrue(isatab.assert_tab_equal(open(os.path.join(source_dir, 'a_gilbert-assay-Tx.txt')),
+        self.assertTrue(isatab.assert_tab_content_equal(open(os.path.join(source_dir, 'a_gilbert-assay-Tx.txt')),
                                                 open(os.path.join(self._tmp, 'a_gilbert-assay-Tx.txt'))))
 
     # def test_isa_bii_s_7(self):
     #     source_dir = os.path.join(self._dir, 'data/BII-S-7/')
     #     isatab2json.convert(source_dir, self._tmp)
     #     json2isatab.convert(open(os.path.join(self._tmp, 'BII-S-7.json')), self._tmp)
-    #     isatab.assert_tab_equal(open(os.path.join(source_dir, 'i_matteo.txt')),
+    #     isatab.assert_tab_content_equal(open(os.path.join(source_dir, 'i_matteo.txt')),
     #                             open(os.path.join(self._tmp, 'i_investigation.txt')))
-    #     isatab.assert_tab_equal(open(os.path.join(source_dir, 's_BII-S-7.txt')),
+    #     isatab.assert_tab_content_equal(open(os.path.join(source_dir, 's_BII-S-7.txt')),
     #                             open(os.path.join(self._tmp, 's_BII-S-7.txt')))
-    #     isatab.assert_tab_equal(open(os.path.join(source_dir, 'a_matteo-assay-Gx.txt')),
+    #     isatab.assert_tab_content_equal(open(os.path.join(source_dir, 'a_matteo-assay-Gx.txt')),
     #                             open(os.path.join(self._tmp, 'a_matteo-assay-Gx.txt')))
-    """
-    File "/Users/dj/PycharmProjects/isa-api/isatools/isajson.py", line 577, in load
-    if parameter_value_json['category']['@id'] == '#parameter/Array_Design_REF':  # Special case
-    KeyError: 'category'
-    """
+    """Problem with intermediate json - Factor Values at assay level"""
