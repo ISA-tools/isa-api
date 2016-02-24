ccbf14e126fc755563d474b4f9758dbe8a0e9354
David Johnson <david.johnson@oerc.ox.ac.uk>
2016-02-24 18:38:45 +0000

    Set up tab2sra test with both s3 and s7 datafiles

--- tests/test_isatab2sra.py
+++ tests/test_isatab2sra.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 import os
 import shutil
+from isatools.convert import isatab2sra
 
 
 class ISATabTest(TestCase):
@@ -8,7 +9,6 @@ class ISATabTest(TestCase):
     def setUp(self):
         """set up directories etc"""
         self._dir = os.path.join(os.path.dirname(__file__), "data")
-        self._work_dir = os.path.join(self._dir, "BII-S-3")
         self._config_dir = os.path.join(self._dir, "Configurations/isaconfig-default_v2015-07-02")
         self._tmp = os.path.join(self._dir, './tmp/')
         if not os.path.exists(self._tmp):
@@ -17,7 +17,12 @@ class ISATabTest(TestCase):
     def tearDown(self):
         shutil.rmtree(self._tmp, ignore_errors=True)
 
-    def test_isatab_to_sra(self):
-        from isatools.convert import isatab2sra
-        isatab2sra.create_sra(self._work_dir, self._tmp, self._config_dir)
+    def test_isatab_to_sra_bii_s_3(self):
+        work_dir = os.path.join(self._dir, "BII-S-3")
+        isatab2sra.create_sra(work_dir, self._tmp, self._config_dir)
+        self.assertTrue(os.path.exists(os.path.join(self._tmp, 'sra')))
+
+    def test_isatab_to_sra_bii_s_7(self):
+        work_dir = os.path.join(self._dir, "BII-S-7")
+        isatab2sra.create_sra(work_dir, self._tmp, self._config_dir)
         self.assertTrue(os.path.exists(os.path.join(self._tmp, 'sra')))
