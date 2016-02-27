41bf3ccef0c0dc70affecf6d91a7a5669b3c0c4a
David Johnson <david.johnson@oerc.ox.ac.uk>
2016-02-27 18:09:30 +0000

    Update setup.py for PyPI publishing

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
+#!/usr/bin/env python
+
 from setuptools import setup
 
 setup(
     name='isatools',
-    version='0.0.4',
-    packages=['isatools', 'isatools.convert', 'isatools.io', 'isatools.model'],
+    version='0.1',
+    packages=['isatools', 'isatools.convert', 'isatools.io', 'isatools.model', 'isatools.sampledata',
+              'isatools.schemas', 'isatools.validate'],
     description='ISA version 1.0 API',
     author='ISA Infrastructure Team',
-    author_email='isa@googlegroups.com',
+    author_email='isatools@googlegroups.com',
+    url='https://github.com/ISA-tools/isa-api',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         ],
-    url='http://www.isa-tools.org/',
-    test_suite='tests'
 )
