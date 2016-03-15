995117cf198e1b49eee3e24c0f9f967dd694106a
David Johnson <david.johnson@oerc.ox.ac.uk>
2016-03-15 14:40:21 +0000

    Add tests for loading any JSON, then ISA JSON

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -1,17 +1,26 @@
-from isatools import isatab
+from unittest import TestCase
 from isatools import isajson
+from isatools import isatab
+import os
 
 
-class ValidateIsaTabTest:
+class ValidateIsaJsonTest(TestCase):
 
     def setUp(self):
-        pass
+        self._dir = os.path.dirname(__file__)
 
     def tearDown(self):
         pass
 
+    def test_invalid_json_load(self):
+        with self.assertRaises(ValueError):
+            isajson.validate(open(os.path.join(self._dir, 'data', 'json', 'invalid.json')))
 
-class ValidateIsaJsonTest:
+    def test_isa_json_load(self):
+        isajson.validate(open(os.path.join(self._dir, 'data', 'json', 'minimal_syntax.json')))
+
+
+class ValidateIsaTabTest:
 
     def setUp(self):
         pass
