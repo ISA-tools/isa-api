e480f4cc9c3e1d9c7550ee2e78eed46a59c9cd7c
David Johnson <david.johnson@oerc.ox.ac.uk>
2016-03-15 12:48:47 +0000

    Add test stubs for JSON validator #98

---
+++ tests/test_validators.py
@@ -0,0 +1,20 @@
+from isatools import isatab
+from isatools import isajson
+
+
+class ValidateIsaTabTest:
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+class ValidateIsaJsonTest:
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
