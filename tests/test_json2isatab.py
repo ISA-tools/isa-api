defcf8c14327f7bc4dfd67a20d4c3397d2917420
David Johnson <david.johnson@oerc.ox.ac.uk>
2016-05-01 13:20:07 +0100

    Fix json2isatab test to check for presence of output files before asserting equality between them

--- tests/test_json2isatab.py
+++ tests/test_json2isatab.py
@@ -18,50 +18,90 @@ class JsonToTabTest(TestCase):
 
     def test_source_split_investigation(self):
         json2isatab.convert(open(os.path.join(self._dir, 'TEST-ISA-source-split.json')), self._tmp)
-        self.assertTrue(assert_tab_content_equal(open(os.path.join(self._tmp, 'i_investigation.txt')),
-                                                 open(os.path.join(self._dir, 'data/TEST-ISA-source-split/i_investigation.txt'))))
+        try:
+            fp = open(os.path.join(self._tmp, 'i_investigation.txt'))
+        except IOError:
+            self.fail("Could not open expected written file i_investigation.txt")
+        if fp is not None:
+            self.assertTrue(assert_tab_content_equal(fp, open(os.path.join(self._dir, 'data/TEST-ISA-source-split/i_investigation.txt'))))
 
     def test_source_split_study_table(self):
         json2isatab.convert(open(os.path.join(self._dir, 'TEST-ISA-source-split.json')), self._tmp)
-        self.assertTrue(assert_tab_content_equal(open(os.path.join(self._tmp, 's_TEST-Template1-Splitting.txt')),
-                                                 open(os.path.join(self._dir, 'data/TEST-ISA-source-split/s_TEST-Template1-Splitting.txt'))))
+        try:
+            fp = open(os.path.join(self._tmp, 's_TEST-Template1-Splitting.txt'))
+        except IOError:
+            self.fail("Could not open expected written file s_TEST-Template1-Splitting.txt")
+        if fp is not None:
+            self.assertTrue(assert_tab_content_equal(fp, open(os.path.join(self._dir, 'data/TEST-ISA-source-split/s_TEST-Template1-Splitting.txt'))))
 
     def test_source_split_assay_table(self):
         json2isatab.convert(open(os.path.join(self._dir, 'TEST-ISA-source-split.json')), self._tmp)
-        self.assertTrue(assert_tab_content_equal(open(os.path.join(self._tmp, 'a_test-template1-splitting_transcription_profiling_DNA_microarray.txt')),
-                                                 open(os.path.join(self._dir, 'data/TEST-ISA-source-split/a_test-template1-splitting_transcription_profiling_DNA_microarray.txt'))))
+        try:
+            fp = open(os.path.join(self._tmp, 'a_test-template1-splitting_transcription_profiling_DNA_microarray.txt'))
+        except IOError:
+            self.fail("Could not open expected written file a_test-template1-splitting_transcription_profiling_DNA_microarray.txt")
+        if fp is not None:
+            self.assertTrue(assert_tab_content_equal(fp, open(os.path.join(self._dir, 'data/TEST-ISA-source-split/a_test-template1-splitting_transcription_profiling_DNA_microarray.txt'))))
 
     def test_sample_pool_investigation(self):
         json2isatab.convert(open(os.path.join(self._dir, 'TEST-ISA-sample-pool.json')), self._tmp)
-        self.assertTrue(assert_tab_content_equal(open(os.path.join(self._tmp, 'i_investigation.txt')),
-                                                 open(os.path.join(self._dir, 'data/TEST-ISA-sample-pool/i_investigation.txt'))))
+        try:
+            fp = open(os.path.join(self._tmp, 'i_investigation.txt'))
+        except IOError:
+            self.fail("Could not open expected written file i_investigation.txt")
+        if fp is not None:
+            self.assertTrue(assert_tab_content_equal(fp, open(os.path.join(self._dir, 'data/TEST-ISA-sample-pool/i_investigation.txt'))))
 
     def test_sample_pool_study_table(self):
         json2isatab.convert(open(os.path.join(self._dir, 'TEST-ISA-sample-pool.json')), self._tmp)
-        self.assertTrue(assert_tab_content_equal(open(os.path.join(self._tmp, 's_TEST-Template3-Splitting.txt')),
-                                                 open(os.path.join(self._dir, 'data/TEST-ISA-sample-pool/s_TEST-Template3-Splitting.txt'))))
+        try:
+            fp = open(os.path.join(self._tmp, 's_TEST-Template3-Splitting.txt'))
+        except IOError:
+            self.fail("Could not open expected written file s_TEST-Template3-Splitting.txt")
+        if fp is not None:
+            self.assertTrue(assert_tab_content_equal(fp, open(os.path.join(self._dir, 'data/TEST-ISA-sample-pool/s_TEST-Template3-Splitting.txt'))))
 
     def test_sample_pool_assay_table(self):
         json2isatab.convert(open(os.path.join(self._dir, 'TEST-ISA-sample-pool.json')), self._tmp)
-        self.assertTrue(assert_tab_content_equal(open(os.path.join(self._tmp, 'a_test-template3-splitting_transcription_profiling_DNA_microarray.txt')),
-                                                 open(os.path.join(self._dir, 'data/TEST-ISA-sample-pool/a_test-template3-splitting_transcription_profiling_DNA_microarray.txt'))))
+        try:
+            fp = open(os.path.join(self._tmp, 'a_test-template3-splitting_transcription_profiling_DNA_microarray.txt'))
+        except IOError:
+            self.fail("Could not open expected written file a_test-template3-splitting_transcription_profiling_DNA_microarray.txt")
+        if fp is not None:
+            self.assertTrue(assert_tab_content_equal(fp, open(os.path.join(self._dir, 'data/TEST-ISA-sample-pool/a_test-template3-splitting_transcription_profiling_DNA_microarray.txt'))))
 
     def test_bii_s_3_investigation(self):
-        json2isatab.convert(open(os.path.join(self._dir, 'data/BII-S-3/BII-S-3.json')), self._tmp)
-        self.assertTrue(assert_tab_content_equal(open(os.path.join(self._tmp, 'i_investigation.txt')),
-                                         open(os.path.join(self._dir, 'data/BII-S-3/i_gilbert.txt'))))
+        try:
+            fp = open(os.path.join(self._tmp, 'i_investigation.txt'))
+            json2isatab.convert(open(os.path.join(self._dir, 'data/BII-S-3/BII-S-3.json')), self._tmp)
+        except IOError:
+            self.fail("Could not open expected written file i_investigation.txt")
+        if fp is not None:
+            self.assertTrue(assert_tab_content_equal(fp, open(os.path.join(self._dir, 'data/BII-S-3/i_gilbert.txt'))))
 
     def test_bii_s_3_study_table(self):
         json2isatab.convert(open(os.path.join(self._dir, 'data/BII-S-3/BII-S-3.json')), self._tmp)
-        self.assertTrue(assert_tab_content_equal(open(os.path.join(self._tmp, 's_BII-S-3.txt')),
-                                         open(os.path.join(self._dir, 'data/BII-S-3/s_BII-S-3.txt'))))
+        try:
+            fp = open(os.path.join(self._tmp, 's_BII-S-3.txt'))
+        except IOError:
+            self.fail("Could not open expected written file s_BII-S-3.txt")
+        if fp is not None:
+            self.assertTrue(assert_tab_content_equal(fp, open(os.path.join(self._dir, 'data/BII-S-3/s_BII-S-3.txt'))))
 
     def test_bii_s_3_assay_table_Tx(self):
         json2isatab.convert(open(os.path.join(self._dir, 'data/BII-S-3/BII-S-3.json')), self._tmp)
-        self.assertTrue(assert_tab_content_equal(open(os.path.join(self._tmp, 'a_gilbert-assay-Tx.txt')),
-                                         open(os.path.join(self._dir, 'data/BII-S-3/a_gilbert-assay-Tx.txt'))))
+        try:
+            fp = open(os.path.join(self._tmp, 'a_gilbert-assay-Tx.txt'))
+        except IOError:
+            self.fail("Could not open expected written file a_gilbert-assay-Tx.txt")
+        if fp is not None:
+            self.assertTrue(assert_tab_content_equal(fp, open(os.path.join(self._dir, 'data/BII-S-3/a_gilbert-assay-Tx.txt'))))
 
     def test_bii_s_3_assay_table_Gx(self):
         json2isatab.convert(open(os.path.join(self._dir, 'data/BII-S-3/BII-S-3.json')), self._tmp)
-        self.assertTrue(assert_tab_content_equal(open(os.path.join(self._tmp, 'a_gilbert-assay-Gx.txt')),
-                                         open(os.path.join(self._dir, 'data/BII-S-3/a_gilbert-assay-Gx.txt'))))
+        try:
+            fp = open(os.path.join(self._tmp, 'a_gilbert-assay-Gx.txt'))
+        except IOError:
+            self.fail("Could not open expected written file a_gilbert-assay-Gx.txt")
+        if fp is not None:
+            self.assertTrue(assert_tab_content_equal(fp, open(os.path.join(self._dir, 'data/BII-S-3/a_gilbert-assay-Gx.txt'))))
