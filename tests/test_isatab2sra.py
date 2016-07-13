635eade70576a25438b4f26930ad8551c23e1bac
David Johnson <david.johnson@oerc.ox.ac.uk>
2016-07-13 12:37:25 +0100

    Improve isatab2sra test to compare generated output from converter with expected data

--- tests/test_isatab2sra.py
+++ tests/test_isatab2sra.py
@@ -4,7 +4,10 @@ from zipfile import ZipFile
 import os
 import shutil
 from isatools.convert import isatab2sra
+import logging
 
+logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class ISATabTest(TestCase):
 
@@ -25,9 +28,73 @@ class ISATabTest(TestCase):
         self.assertIsInstance(res, BytesIO)
 
         # check that content was effectively written to response
-        with ZipFile(res) as zip_file:
+        with ZipFile(res) as zip_file:  # isatab2sra returns a zip
             self.assertGreater(len(zip_file.namelist()), 0)
-        self.assertTrue(os.path.exists(os.path.join(self._tmp, 'sra')))
+        self.assertTrue(os.path.exists(os.path.join(self._tmp, 'sra', 'BII-S-3')))  # but also writes to /sra/{study_id}
+
+        # SRA should always produce experiment_set.xml, run_set.xml, sample_set.xml study.xml and submission.xml
+        expected_sra_path = os.path.join(self._tmp, 'sra', 'BII-S-3')
+        expected_file_set = {'experiment_set.xml', 'run_set.xml', 'sample_set.xml', 'study.xml', 'submission.xml'}
+        if os.path.exists(expected_sra_path):
+            sra_output_files_list = set(os.listdir(expected_sra_path))
+            extra_files_found = sra_output_files_list - expected_file_set
+            if len(extra_files_found) > 0:
+                self.fail("Unexpected file found in SRA output: " + str(extra_files_found))
+            expected_files_missing = expected_file_set - sra_output_files_list
+            if len(expected_files_missing) > 0:
+                self.fail("Unexpected file found in SRA output: " + str(expected_files_missing))
+            # Now try load the SRA output in test and compare against the expected output in test data directory
+            from lxml import objectify, etree
+
+            # compare experiment_set.xml
+            test_experiment_set_file = open(os.path.join(expected_sra_path, 'experiment_set.xml'), 'rb')
+            logger.info("Opening: " + test_experiment_set_file.name)
+            test_experiment_set_xml_obj = objectify.fromstring(test_experiment_set_file.read())
+            expected_experiment_set_file = open(os.path.join(work_dir, 'sra', 'experiment_set.xml'), 'rb')
+            logger.info("Opening: " + expected_experiment_set_file.name)
+            expected_experiment_set_xml_obj = objectify.fromstring(expected_experiment_set_file.read())
+            logger.info("Comparing output of SRA conversion to expected test data...")
+            self.assertEqual(etree.tostring(test_experiment_set_xml_obj), etree.tostring(expected_experiment_set_xml_obj))
+
+            # compare run_set.xml
+            test_run_set_file = open(os.path.join(expected_sra_path, 'run_set.xml'), 'rb')
+            logger.info("Opening: " + test_run_set_file.name)
+            test_run_set_xml_obj = objectify.fromstring(test_run_set_file.read())
+            expected_run_set_file = open(os.path.join(work_dir, 'sra', 'run_set.xml'), 'rb')
+            logger.info("Opening: " + expected_run_set_file.name)
+            expected_run_set_xml_obj = objectify.fromstring(expected_run_set_file.read())
+            logger.info("Comparing output of SRA conversion to expected test data...")
+            self.assertEqual(etree.tostring(test_run_set_xml_obj), etree.tostring(expected_run_set_xml_obj))
+
+            # compare sample_set.xml
+            test_sample_set_file = open(os.path.join(expected_sra_path, 'sample_set.xml'), 'rb')
+            logger.info("Opening: " + test_sample_set_file.name)
+            test_sample_set_xml_obj = objectify.fromstring(test_sample_set_file.read())
+            expected_sample_set_file = open(os.path.join(work_dir, 'sra', 'sample_set.xml'), 'rb')
+            logger.info("Opening: " + expected_sample_set_file.name)
+            expected_sample_set_xml_obj = objectify.fromstring(expected_sample_set_file.read())
+            logger.info("Comparing output of SRA conversion to expected test data...")
+            self.assertEqual(etree.tostring(test_sample_set_xml_obj), etree.tostring(expected_sample_set_xml_obj))
+
+            # compare study.xml
+            test_study_file = open(os.path.join(expected_sra_path, 'study.xml'), 'rb')
+            logger.info("Opening: " + test_study_file.name)
+            test_study_xml_obj = objectify.fromstring(test_study_file.read())
+            expected_study_file = open(os.path.join(work_dir, 'sra', 'study.xml'), 'rb')
+            logger.info("Opening: " + expected_study_file.name)
+            expected_study_xml_obj = objectify.fromstring(expected_study_file.read())
+            logger.info("Comparing output of SRA conversion to expected test data...")
+            self.assertEqual(etree.tostring(test_study_xml_obj), etree.tostring(expected_study_xml_obj))
+
+            # compare submission.xml
+            test_submission_file = open(os.path.join(expected_sra_path, 'submission.xml'), 'rb')
+            logger.info("Opening: " + test_submission_file.name)
+            test_submission_xml_obj = objectify.fromstring(test_submission_file.read())
+            expected_submission_file = open(os.path.join(work_dir, 'sra', 'submission.xml'), 'rb')
+            logger.info("Opening: " + expected_submission_file.name)
+            expected_submission_xml_obj = objectify.fromstring(expected_submission_file.read())
+            logger.info("Comparing output of SRA conversion to expected test data...")
+            self.assertEqual(etree.tostring(test_submission_xml_obj), etree.tostring(expected_submission_xml_obj))
 
     def test_isatab_to_sra_bii_s_7(self):
         work_dir = os.path.join(self._dir, "BII-S-7")
