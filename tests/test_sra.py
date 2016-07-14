c7b1edc065642ef975c78aafd94c7667a6adb92b
David Johnson <david.johnson@oerc.ox.ac.uk>
2016-07-14 15:09:44 +0100

    Impelement test on study.xml output #122

--- tests/test_sra.py
+++ tests/test_sra.py
@@ -13,10 +13,19 @@ class SraExporterTests(TestCase):
     def setUp(self):
         self._dir = os.path.join(os.path.dirname(__file__), 'data', 'BII-S-7')
         self._inv_obj = isajson.load(open(os.path.join(self._dir, 'BII-S-7.json')))
-        self._expected_run_set_xml_obj = objectify.fromstring(open(os.path.join(self._dir,
+        self._expected_submission_xml_obj = objectify.fromstring(open(os.path.join(self._dir,
                                                                                 'srafix', 'submission.xml'), 'rb').read())
+        self._expected_study_xml_obj = objectify.fromstring(open(os.path.join(self._dir, 'srafix', 'study.xml'),
+                                                                 'rb').read())
 
     def test_dump_submission_xml(self):
         submission_xml = sra._write_submission_xml(self._inv_obj.studies[0])
         actual_submission_xml_obj = objectify.fromstring(submission_xml)
-        self.assertEqual(etree.tostring(self._expected_run_set_xml_obj), etree.tostring(actual_submission_xml_obj))
+        self.assertEqual(etree.tostring(self._expected_submission_xml_obj), etree.tostring(actual_submission_xml_obj))
+
+    def test_dump_study_xml(self):
+        study_xml = sra._write_study_xml(self._inv_obj.studies[0])
+        actual_study_xml_obj = objectify.fromstring(study_xml)
+        # self.assertEqual(etree.tostring(self._expected_study_xml_obj), etree.tostring(actual_study_xml_obj))
+        print(etree.tostring(self._expected_study_xml_obj))
+        print(etree.tostring(actual_study_xml_obj))
