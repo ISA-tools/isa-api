1c61b59fe999757135682ee828c04855578c2ef1
David Johnson <david.johnson@oerc.ox.ac.uk>
2016-07-14 16:56:43 +0100

    Fix refactoring in submission.xml writer #122

--- isatools/sra.py
+++ isatools/sra.py
@@ -49,7 +49,11 @@ def _get_study_type(assays):
         return 'Other'
 
 
-def _write_submission_xml(s):
+def _write_submission_xml(i):
+    if len(i.studies) > 1:
+        raise AttributeError("Detected more than one study in investigation. For an SRA submission, only one study per "
+                             "ISA submission can be handled.")
+    s = i.studies[0]
     sra_center_name = _get_comment(s.comments, 'SRA Center Name')
     sra_broker_name = _get_comment(s.comments, 'SRA Broker Name')
     sra_contact = _get_sra_contact(s.contacts)
@@ -88,7 +92,11 @@ def _write_submission_xml(s):
     return submission_xml
 
 
-def _write_study_xml(s):
+def _write_study_xml(i):
+    if len(i.studies) > 1:
+        raise AttributeError("Detected more than one study in investigation. For an SRA submission, only one study per "
+                             "ISA submission can be handled.")
+    s = i.studies[0]
     sra_center_name = _get_comment(s.comments, 'SRA Center Name')
     sra_center_project_name = _get_comment(s.comments, 'SRA Center Project Name')
     study_type = _get_study_type(s.assays)
@@ -139,56 +147,70 @@ def _write_study_xml(s):
     study_xml += link_xml
     # build STUDY ATTRIBUTES
     attr_xml = """<STUDY_ATTRIBUTES>"""
-    if s.submission_date is not None:
+    if s.submission_date is not None and s.submission_date != '':
         attr_xml += """
         <STUDY_ATTRIBUTE>
             <TAG>Submission Date</TAG>
             <VALUE>{submission_date}</VALUE>
         </STUDY_ATTRIBUTE>
         """.format(submission_date=s.submission_date)
-    if s.public_release_date is not None:
+    if s.public_release_date is not None and s.public_release_date != '':
         attr_xml += """
         <STUDY_ATTRIBUTE>
             <TAG>Release Date</TAG>
             <VALUE>{public_release_date}</VALUE>
         </STUDY_ATTRIBUTE>
         """.format(public_release_date=s.public_release_date)
+    if s.identifier is not None and s.identifier != '':
+        attr_xml += """
+        <STUDY_ATTRIBUTE>
+            <TAG>BII Study Accession</TAG>
+            <VALUE>{study_id}</VALUE>
+        </STUDY_ATTRIBUTE>
+        """.format(study_id=s.identifier)
+    if i.identifier is not None and i.identifier != '':
+        attr_xml += """
+        <STUDY_ATTRIBUTE>
+            <TAG>BII Investigation Accession</TAG>
+            <VALUE>{inv_id}</VALUE>
+        </STUDY_ATTRIBUTE>
+        """.format(inv_id=i.identifier)
     for contact in s.contacts:
-        contact_info = """
-Name: {contact_name}
-e-mail: {contact_email}
-Affiliation: {contact_affiliation}
-Address: {contact_address}
-        """.format(contact_name=contact.first_name + ' ' + contact.last_name,
-                   contact_email=contact.email,
-                   contact_affiliation=contact.affiliation,
-                   contact_address=contact.address)
+        contact_txt = """"""
+        if contact.first_name is not None and contact.first_name != '' and contact.last_name is not None and contact.last_name != '':
+            contact_txt += "Name: {first} {last}\n".format(first=contact.first_name, last=contact.last_name)
+        if contact.email is not None and contact.email != '':
+            contact_txt += "e-mail: {email}\n".format(email=contact.email)
+        if contact.affiliation is not None and contact.affiliation != '':
+            contact_txt += "Affiliation: {affiliation}\n".format(affiliation=contact.affiliation)
+        if contact.address is not None and contact.address != '':
+            contact_txt += "Address: {address}\n".format(address=contact.address)
         for role in contact.roles:
-            contact_info += """Role: {role}""".format(role=role.name)
+            contact_txt += "Role: {role}\n".format(role=role.name)
         attr_xml += """
         <STUDY_ATTRIBUTE>
             <TAG>Study Contact</TAG>
             <VALUE>{contact_info}</VALUE>
         </STUDY_ATTRIBUTE>
-        """.format(contact_info=contact_info)
+        """.format(contact_info=contact_txt[:len(contact_txt)-1])  # drop trailing line break
     for publication in s.publications:
+        pub_txt = """"""
+        if publication.title is not None and publication.title != '':
+            pub_txt += "Title: {title}\n".format(title=publication.title.strip())
+        if publication.author_list is not None and publication.author_list != '':
+            pub_txt += "Authors: {author_list}\n".format(author_list=publication.author_list.strip())
+        if publication.status is not None and publication.status != '':
+            pub_txt += "Status: {status}\n".format(status=publication.status.name.strip())
+        if publication.pubmed_id is not None and publication.pubmed_id != '':
+            pub_txt += "PUBMED ID: {pubmed_id}\n".format(pubmed_id=publication.pubmed_id.strip())
+        if publication.doi is not None and publication.doi != '':
+            pub_txt += "DOI: {doi}\n".format(doi=publication.doi.strip())
         attr_xml += """
         <STUDY_ATTRIBUTE>
             <TAG>Study Publication</TAG>
-            <VALUE>
-Title: {title}
-Authors: {author_list}
-Status: {status}
-PUBMED ID: {pubmed_id}
-DOI: {doi}</VALUE>
+            <VALUE>{pub_txt}</VALUE>
         </STUDY_ATTRIBUTE>
-        """.format(
-            title=publication.title,
-            author_list=publication.author_list,
-            status=publication.status.name,
-            pubmed_id=publication.pubmed_id,
-            doi=publication.doi
-        )
+        """.format(pub_txt=pub_txt[:len(pub_txt)-1])  # drop trailing line break
     attr_xml += """</STUDY_ATTRIBUTES>"""
     study_xml += attr_xml
     study_xml += """</STUDY>"""
@@ -215,12 +237,10 @@ def dump(isa_obj, output_path):
         >>> i = isajson.load(open('.../BII-S-7.json'))
         >>> sra.dump(i, None)
     """
-    for study in isa_obj.studies:
+    submission_xml = _write_submission_xml(isa_obj)
+    logger.info(submission_xml)
+    etree.fromstring(submission_xml)
 
-        submission_xml = _write_submission_xml(study)
-        logger.info(submission_xml)
-        etree.fromstring(submission_xml)
-
-        study_xml = _write_study_xml(study)
-        logger.info(study_xml)
-        etree.fromstring(study_xml)  # checks if validates against XSD
+    study_xml = _write_study_xml(isa_obj)
+    logger.info(study_xml)
+    etree.fromstring(study_xml)  # checks if validates against XSD
