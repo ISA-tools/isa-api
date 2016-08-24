54448731284868e5968bd5f1bfd911d3828b932d
David Johnson <david.johnson@oerc.ox.ac.uk>
2016-08-24 16:54:03 +0100

    Update tests to use new method behaviour from #140;  #132

--- tests/test_json2isatab2json_convert.py
+++ tests/test_json2isatab2json_convert.py
@@ -11,10 +11,6 @@ class TestJsonIsaTabTwoWayConvert(unittest.TestCase):
 
     def setUp(self):
         self._json_data_dir = utils.JSON_DATA_DIR
-        self._unit_json_data_dir = utils.UNIT_JSON_DATA_DIR
-        self._configs_json_data_dir = utils.JSON_DEFAULT_CONFIGS_DATA_DIR
-        self._sra_data_dir = utils.SRA_DATA_DIR
-        self._sra_configs_dir = utils.DEFAULT2015_XML_CONFIGS_DATA_DIR
         self._tmp_dir = tempfile.mkdtemp()
 
     def tearDown(self):
@@ -24,20 +20,18 @@ class TestJsonIsaTabTwoWayConvert(unittest.TestCase):
         test_case = 'TEST-ISA-sample-pool'
         test_json = open(os.path.join(self._json_data_dir, test_case + '.json'))
         json2isatab.convert(test_json, self._tmp_dir)
-        isatab2json.convert(self._tmp_dir, self._tmp_dir)
         test_json.seek(0)  # reset pointer
         expected_json = json.load(test_json)
-        actual_json = json.load(open(os.path.join(self._tmp_dir, test_case + '.json')))
+        actual_json = isatab2json.convert(self._tmp_dir)
         self.assertTrue(utils.assert_json_equal(expected_json, actual_json))
 
     def test_json2isatab_isatab2json_2way_convert_source_split(self):
         test_case = 'TEST-ISA-source-split'
         test_json = open(os.path.join(self._json_data_dir, test_case + '.json'))
         json2isatab.convert(test_json, self._tmp_dir)
-        isatab2json.convert(self._tmp_dir, self._tmp_dir)
         test_json.seek(0)  # reset pointer
         expected_json = json.load(test_json)
-        actual_json = json.load(open(os.path.join(self._tmp_dir, test_case + '.json')))
+        actual_json = isatab2json.convert(self._tmp_dir)
         self.assertTrue(utils.assert_json_equal(expected_json, actual_json))
 
     def test_json2isatab_isatab2json_2way_convert_repeated_measure(self):
@@ -45,10 +39,9 @@ class TestJsonIsaTabTwoWayConvert(unittest.TestCase):
         test_case = 'TEST-ISA-repeated-measure'
         test_json = open(os.path.join(self._json_data_dir, test_case + '.json'))
         json2isatab.convert(test_json, self._tmp_dir)
-        isatab2json.convert(self._tmp_dir, self._tmp_dir)
         test_json.seek(0)  # reset pointer
         expected_json = json.load(test_json)
-        actual_json = json.load(open(os.path.join(self._tmp_dir, test_case + '.json')))
+        actual_json = isatab2json.convert(self._tmp_dir)
         self.assertTrue(utils.assert_json_equal(expected_json, actual_json))
 
     def test_json2isatab_isatab2json_2way_convert_charac_param_factor(self):
@@ -58,10 +51,9 @@ class TestJsonIsaTabTwoWayConvert(unittest.TestCase):
         test_case = 'TEST-ISA-charac-param-factor'
         test_json = open(os.path.join(self._json_data_dir, test_case + '.json'))
         json2isatab.convert(test_json, self._tmp_dir)
-        isatab2json.convert(self._tmp_dir, self._tmp_dir)
         test_json.seek(0)  # reset pointer
         expected_json = json.load(test_json)
-        actual_json = json.load(open(os.path.join(self._tmp_dir, test_case + '.json')))
+        actual_json = isatab2json.convert(self._tmp_dir)
         self.assertTrue(utils.assert_json_equal(expected_json, actual_json))
 
     def test_json2isatab_isatab2json_2way_convert_bii_i_1(self):
@@ -73,10 +65,9 @@ class TestJsonIsaTabTwoWayConvert(unittest.TestCase):
         test_case = 'BII-I-1'
         test_json = open(os.path.join(self._json_data_dir, test_case, test_case + '.json'))
         json2isatab.convert(test_json, self._tmp_dir)
-        isatab2json.convert(self._tmp_dir, self._tmp_dir)
         test_json.seek(0)  # reset pointer
         expected_json = json.load(test_json)
-        actual_json = json.load(open(os.path.join(self._tmp_dir, test_case + '.json')))
+        actual_json = isatab2json.convert(self._tmp_dir)
         self.assertTrue(utils.assert_json_equal(expected_json, actual_json))
 
     def test_json2isatab_isatab2json_2way_convert_bii_s_3(self):
@@ -88,10 +79,9 @@ class TestJsonIsaTabTwoWayConvert(unittest.TestCase):
         test_case = 'BII-S-3'
         test_json = open(os.path.join(self._json_data_dir, test_case, test_case + '.json'))
         json2isatab.convert(test_json, self._tmp_dir)
-        isatab2json.convert(self._tmp_dir, self._tmp_dir)
         test_json.seek(0)  # reset pointer
         expected_json = json.load(test_json)
-        actual_json = json.load(open(os.path.join(self._tmp_dir, test_case + '.json')))
+        actual_json = isatab2json.convert(self._tmp_dir)
         self.assertTrue(utils.assert_json_equal(expected_json, actual_json))
 
     def test_json2isatab_isatab2json_2way_convert_bii_s_7(self):
@@ -101,8 +91,7 @@ class TestJsonIsaTabTwoWayConvert(unittest.TestCase):
         test_case = 'BII-S-7'
         test_json = open(os.path.join(self._json_data_dir, test_case, test_case + '.json'))
         json2isatab.convert(test_json, self._tmp_dir)
-        isatab2json.convert(self._tmp_dir, self._tmp_dir)
         test_json.seek(0)  # reset pointer
         expected_json = json.load(test_json)
-        actual_json = json.load(open(os.path.join(self._tmp_dir, test_case + '.json')))
+        actual_json = isatab2json.convert(self._tmp_dir)
         self.assertTrue(utils.assert_json_equal(expected_json, actual_json))
