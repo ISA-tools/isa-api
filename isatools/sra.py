a5f5c6babe5ec1a114977b8d6af1f7408eca9de7
David Johnson <david.johnson@oerc.ox.ac.uk>
2016-10-11 16:41:37 +0100

    Add tests to SRA MD5 hasher #152

--- isatools/sra.py
+++ isatools/sra.py
@@ -414,5 +414,5 @@ def create_datafile_hashes(fileroot, filenames):
         if isfile(join(fileroot, file)):
             datafilehashes[file] = md5sum(filename=join(fileroot, file))
         else:
-            raise FileNotFoundError("{} is not a file".format(fileroot + file))
+            raise FileNotFoundError("{} is not a file".format(join(fileroot, file)))
     return datafilehashes
