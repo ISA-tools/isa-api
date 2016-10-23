79110afa0db34a3c3c479784e9728dbf069b7b99
Martin Larralde <martin.larralde@ens-cachan.fr>
2016-10-23 22:44:58 +0100

    Use contexts in test_import_sampledata

--- tests/test_import_sampledata.py
+++ tests/test_import_sampledata.py
@@ -11,19 +11,22 @@ class TestImportSampleData(unittest.TestCase):
 
     def test_import_bii_i_1(self):
         test_case = 'BII-I-1'
-        json_expected = json.load(open(os.path.join(utils.JSON_DATA_DIR, test_case, test_case + '.json')))
+        with open(os.path.join(utils.JSON_DATA_DIR, test_case, test_case + '.json')) as json_file:
+            json_expected = json.load(json_file)
         from isatools.sampledata import bii_i_1
         utils.assert_json_equal(json_expected, bii_i_1.json)
 
 
     def test_import_bii_s_3(self):
         test_case = 'BII-S-3'
-        json_expected = json.load(open(os.path.join(utils.JSON_DATA_DIR, test_case, test_case + '.json')))
+        with open(os.path.join(utils.JSON_DATA_DIR, test_case, test_case + '.json')) as json_file:
+            json_expected = json.load(json_file)
         from isatools.sampledata import bii_s_3
         utils.assert_json_equal(json_expected, bii_s_3.json)
 
     def test_import_bii_s_7(self):
         test_case = 'BII-S-7'
-        json_expected = json.load(open(os.path.join(utils.JSON_DATA_DIR, test_case, test_case + '.json')))
+        with open(os.path.join(utils.JSON_DATA_DIR, test_case, test_case + '.json')) as json_file:
+            json_expected = json.load(json_file)
         from isatools.sampledata import bii_s_7
         utils.assert_json_equal(json_expected, bii_s_7.json)
