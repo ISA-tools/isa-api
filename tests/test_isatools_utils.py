31fd8b1122c15c9fd8dea5593154893ef38ca090
David Johnson <david.johnson@oerc.ox.ac.uk>
2016-11-28 22:28:20 +0000

    Add batch MetaboLights pooling test script #146 #160

--- tests/test_isatools_utils.py
+++ tests/test_isatools_utils.py
@@ -1,7 +1,11 @@
 import unittest
 import os
+import json
 from isatools import utils, isajson
 from tests import utils as test_utils
+from isatools.io import mtbls as MTBLS
+from io import StringIO
+from jsonschema.exceptions import ValidationError
 
 
 class TestIsaGraph(unittest.TestCase):
@@ -14,3 +18,24 @@ class TestIsaGraph(unittest.TestCase):
             for assay in study.assays:
                 print("Checking {}".format(assay.filename))
                 utils.detect_graph_process_pooling(assay.graph)
+
+    def test_detect_graph_process_pooling_batch_on_mtbls(self):
+        for i in range(1, 1):
+            try:
+                print("Loading MTBLS{}".format(i))
+                J = MTBLS.getj("MTBLS{}".format(i))
+                ISA = isajson.load(StringIO(json.dumps(J)))
+                for study in ISA.studies:
+                    print("Checking {}".format(study.filename))
+                    utils.detect_graph_process_pooling(study.graph)
+                    for assay in study.assays:
+                        print("Checking {}".format(assay.filename))
+                        utils.detect_graph_process_pooling(assay.graph)
+            except IOError:
+                print("IO Error, skipping...")
+            except KeyError:
+                print("KeyError, skipping...")
+            except AttributeError:
+                print("AttributeError, skipping...")
+            except ValidationError:
+                print("jsonschema ValidationError, skipping...")
