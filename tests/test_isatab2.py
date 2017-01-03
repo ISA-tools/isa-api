6a1aa4864ad2c7b005bf7487c9baaa9aa6df18d1
David Johnson <david.johnson@oerc.ox.ac.uk>
2017-01-03 09:11:32 +0000

    Add isatab2 test on BII-S-3 Gx #168

--- tests/test_isatab2.py
+++ tests/test_isatab2.py
@@ -30,5 +30,19 @@ class TestIsaTab2JsonIdentifierName(unittest.TestCase):
         self.assertEqual(len(samples), 29)  # expecting 29 samples
         self.assertEqual(len(other_material), 29)  # expecting 29 extracts
         self.assertEqual(len(data), 29)  # expecting 29 raw data files
-        self.assertEqual(len(processes), 116)  # expecting 18 processes (29 sets of 4)
+        self.assertEqual(len(processes), 116)  # expecting 118 processes (29 sets of 4)
         self.assertEqual(len(process_sequences), 29)  # expecting 29 process sequences
+        #  TODO: Fix processes and sequences to combine repeated parts to build graph properly.
+
+    def test_isatab2_bii_s_3_Gx(self):
+        test_case = 'BII-S-3'
+        df = isatab2.read_tfile(os.path.join(self._tab_data_dir, test_case, 'a_gilbert-assay-Gx.txt'))
+        sources, samples, other_material, data, processes, process_sequences = isatab2.ProcessSequenceFactory().create_from_df(df)
+        print(sources, samples, other_material, data, processes, process_sequences)
+        self.assertEqual(len(sources), 0)  # expecting no sources
+        self.assertEqual(len(samples), 4)  # expecting 4 samples
+        self.assertEqual(len(other_material), 4)  # expecting 4 extracts
+        self.assertEqual(len(data), 6)  # expecting 6 raw data files
+        self.assertEqual(len(processes), 24)  # expecting 48 processes (6 sets of 4)
+        self.assertEqual(len(process_sequences), 6)  # expecting 6 process sequences
+        #  TODO: Fix processes and sequences to combine repeated parts to build graph properly
