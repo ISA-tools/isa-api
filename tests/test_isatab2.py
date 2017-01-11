23328adb1ab900c5e784337a85fa43ccafdc5e51
David Johnson <david.johnson@oerc.ox.ac.uk>
2017-01-11 17:30:37 +0000

    Fix build

--- tests/test_isatab2.py
+++ tests/test_isatab2.py
@@ -100,8 +100,8 @@ class TestIsaTab2(unittest.TestCase):
     def test_isatab2_charac_param_factor(self):
         test_case = 'TEST-ISA-charac-param-factor'
         df = isatab2.read_tfile(os.path.join(self._tab_data_dir, test_case, 'a_test-template1-splitting_transcription_profiling_DNA_microarray.txt'))
-        sources, samples, other_material, data, processes, process_sequences = isatab2.ProcessSequenceFactory().create_from_df(df)
-        print(sources, samples, other_material, data, processes, process_sequences)
+        sources, samples, other_material, data, processes = isatab2.ProcessSequenceFactory().create_from_df(df)
+        print(sources, samples, other_material, data, processes)
         self.assertEqual(len(sources), 0)  # expecting no sources
         self.assertEqual(len(samples), 2)  # expecting 2 samples
         self.assertEqual(len(other_material), 4)  # expecting 2 extracts, 2 labeled extracts
@@ -112,8 +112,8 @@ class TestIsaTab2(unittest.TestCase):
     def test_isatab2_bii_s_7(self):
         test_case = 'BII-S-7'
         df = isatab2.read_tfile(os.path.join(self._tab_data_dir, test_case, 'a_matteo-assay-Gx.txt'))
-        sources, samples, other_material, data, processes, process_sequences = isatab2.ProcessSequenceFactory().create_from_df(df)
-        print(sources, samples, other_material, data, processes, process_sequences)
+        sources, samples, other_material, data, processes = isatab2.ProcessSequenceFactory().create_from_df(df)
+        print(sources, samples, other_material, data, processes)
         self.assertEqual(len(sources), 0)  # expecting no sources
         self.assertEqual(len(samples), 29)  # expecting 29 samples
         self.assertEqual(len(other_material), 29)  # expecting 29 extracts
@@ -125,8 +125,8 @@ class TestIsaTab2(unittest.TestCase):
     def test_isatab2_bii_s_3_Gx(self):
         test_case = 'BII-S-3'
         df = isatab2.read_tfile(os.path.join(self._tab_data_dir, test_case, 'a_gilbert-assay-Gx.txt'))
-        sources, samples, other_material, data, processes, process_sequences = isatab2.ProcessSequenceFactory().create_from_df(df)
-        print(sources, samples, other_material, data, processes, process_sequences)
+        sources, samples, other_material, data, processes = isatab2.ProcessSequenceFactory().create_from_df(df)
+        print(sources, samples, other_material, data, processes)
         self.assertEqual(len(sources), 0)  # expecting no sources
         self.assertEqual(len(samples), 4)  # expecting 4 samples
         self.assertEqual(len(other_material), 4)  # expecting 4 extracts
