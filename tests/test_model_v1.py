fcfdcb2f73adc41a7f1705403785ad89bde45abf
David Johnson <david.johnson@oerc.ox.ac.uk>
2017-01-16 09:09:30 +0000

    Refactor/add tests on model

---
+++ tests/test_model_v1.py
@@ -0,0 +1,48 @@
+import unittest
+
+
+class TestSimpleExamples(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_construct_investigation_object(self):
+        try:
+            from isatools.model.v1 import Investigation
+        except ImportError:
+            self.fail("Could not import Investigation class")
+        I = Investigation()
+        self.assertIsInstance(I.identifier, str)
+        self.assertIsInstance(I.title, str)
+        self.assertIsInstance(I.description, str)
+        self.assertIsInstance(I.submission_date, str)  # todo: check if ISO8601
+        self.assertIsInstance(I.public_release_date, str)  # todo: check if ISO8601
+
+    def test_batch_create_materials(self):
+        try:
+            from isatools.model.v1 import Source
+        except ImportError:
+            self.fail("Could not import Source class")
+        source = Source(name='source_material')
+
+        try:
+            from isatools.model.v1 import Sample
+        except ImportError:
+            self.fail("Could not import Sample class")
+        prototype_sample = Sample(name='sample_material', derives_from=source)
+
+        try:
+            from isatools.model.v1 import batch_create_materials
+        except ImportError:
+            self.fail("Could not import batch_create_materials function")
+
+        batch = batch_create_materials(prototype_sample, n=3)
+        self.assertEqual(len(batch), 3)
+        for material in batch:
+            self.assertIsInstance(material, Sample)
+            self.assertEqual(material.derives_from, source)
+        self.assertSetEqual(set([m.name for m in batch]), {'sample_material-0', 'sample_material-1',
+                                                           'sample_material-2'})
