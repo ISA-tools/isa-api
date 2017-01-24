b4f288a4bf29e3513f385bd416b22a602e07b8c3
David Johnson <david.johnson@oerc.ox.ac.uk>
2017-01-24 17:09:41 +0000

    Implements #157

--- isatools/utils.py
+++ isatools/utils.py
@@ -92,3 +92,51 @@ def contains(small_list, big_list):
         else:
             return i, i + len(small_list)
     return False
+
+
+def get_pubmed_article(pubmed_id):
+    # http://biopython.org/DIST/docs/tutorial/Tutorial.html#htoc126
+    from Bio import Entrez, Medline
+    response = {}
+    Entrez.email = 'isatools@googlegroups.com'
+    handle = Entrez.efetch(db="pubmed", id=pubmed_id.strip(), rettype="medline", retmode="text")
+    records = Medline.parse(handle)
+    for record in records:
+        response['pubmedid'] = pubmed_id
+        response['title'] = record.get("TI", "")
+        response['authors'] = record.get("AU", "")
+        response['journal'] = record.get("TA", "")
+        response['year'] = record.get("EDAT", "").split('/')[0]
+        lidstring = record.get("LID", "")
+        if '[doi]' in lidstring:
+            response['doi'] = record.get("LID", "").split(' ')[0]
+        else:
+            response['doi'] = ''
+        if not response['doi']:
+            aids = record.get("AID", "")
+            for aid in aids:
+                print("AID:" + aid)
+                if '[doi]' in aid:
+                    response['doi'] = aid.split(' ')[0]
+                    break
+                else:
+                    response['doi'] = ''
+
+        break
+    return response
+
+
+def set_pubmed_article(publication):
+    """
+        Given a Publication object with pubmed_id set to some value, set the rest of the values from information
+        collected via Entrez webservice from PubMed
+    """
+    from isatools.model.v1 import Publication, Comment
+    if isinstance(publication, Publication):
+        response = get_pubmed_article(publication.pubmed_id)
+        publication.doi = response['doi']
+        publication.author_list = ", ".join(response['authors'])
+        publication.title = response['title']
+        publication.comments = [Comment(name='Journal', value=response['journal'])]
+    else:
+        raise TypeError("Can only set PubMed details on a Publication object")
