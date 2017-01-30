dbd0e0df34253e7ef1adeb07309bacf637eb6556
David Johnson <david.johnson@oerc.ox.ac.uk>
2017-01-30 23:11:34 +0000

    Implement tests on ISArchive feature, closes #180

--- tests/test_isatools_utils.py
+++ tests/test_isatools_utils.py
@@ -8,6 +8,8 @@ from jsonschema.exceptions import ValidationError
 from tests import utils as test_utils
 from isatools import utils
 from isatools.model.v1 import OntologySource, OntologyAnnotation
+import tempfile
+import shutil
 
 
 def setUpModule():
@@ -79,3 +81,45 @@ class TestOlsSearch(unittest.TestCase):
         self.assertEqual(ontology_anotation.term, "cell type")
         self.assertEqual(ontology_anotation.term_accession, "http://www.ebi.ac.uk/efo/EFO_0000324")
         self.assertEqual(ontology_anotation.term_source, ontology_source)
+
+
+class TestISArchiveExport(unittest.TestCase):
+
+    def setUp(self):
+        self._tmp_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self._tmp_dir)
+
+    def test_create_isatab_archive_missing_files(self):
+        with open(os.path.join(test_utils.TAB_DATA_DIR, "BII-I-1", "i_investigation.txt")) as fp:
+            result = utils.create_isatab_archive(inv_fp=fp)
+            self.assertIsNone(result)  # returns None if can't create archive
+
+    def test_create_isatab_archive(self):
+        with open(os.path.join(test_utils.TAB_DATA_DIR, "BII-S-3", "i_gilbert.txt")) as fp:
+            result = utils.create_isatab_archive(inv_fp=fp)
+            self.assertIsInstance(result, list)
+            self.assertListEqual(sorted(result),
+                                 sorted(['i_gilbert.txt', 's_BII-S-3.txt', 'a_gilbert-assay-Gx.txt',
+                                         'a_gilbert-assay-Tx.txt', 'EXHS9OF02.sff', 'EXHS9OF01.sff',
+                                         'EWOEPZA01.sff', 'EWOEPZA02.sff', 'EX398L101.sff', 'EX398L102.sff',
+                                         'EVHINN105.sff', 'EVNG8PH04.sff', 'EVUSNDQ01.sff', 'EVHINN102.sff',
+                                         'EVHINN113.sff', 'EVUSNDQ02.sff', 'EVHINN101.sff', 'EVNG8PH01.sff',
+                                         'EVHINN106.sff', 'EVHINN108.sff', 'EVHINN116.sff', 'EVHINN104.sff',
+                                         'EVHINN110.sff', 'EVHINN107.sff', 'EVUSNDQ03.sff', 'EVHINN103.sff',
+                                         'EVHINN112.sff', 'EVUSNDQ04.sff', 'EVHINN115.sff', 'EVNG8PH02.sff',
+                                         'EVHINN114.sff', 'EVHINN111.sff', 'EVNG8PH03.sff', 'EVHINN109.sff']))
+
+    def test_create_isatab_archive_filter_on_transcription_profiling(self):
+        with open(os.path.join(test_utils.TAB_DATA_DIR, "BII-S-3", "i_gilbert.txt")) as fp:
+            result = utils.create_isatab_archive(inv_fp=fp, filter_by_measurement='transcription profiling')
+            self.assertIsInstance(result, list)
+            self.assertListEqual(sorted(result),
+                                 sorted(['i_gilbert.txt', 's_BII-S-3.txt', 'a_gilbert-assay-Tx.txt', 'EVHINN105.sff',
+                                         'EVNG8PH04.sff', 'EVUSNDQ01.sff', 'EVHINN102.sff', 'EVHINN113.sff',
+                                         'EVUSNDQ02.sff', 'EVHINN101.sff', 'EVNG8PH01.sff', 'EVHINN106.sff',
+                                         'EVHINN108.sff', 'EVHINN116.sff', 'EVHINN104.sff', 'EVHINN110.sff',
+                                         'EVHINN107.sff', 'EVUSNDQ03.sff', 'EVHINN103.sff', 'EVHINN112.sff',
+                                         'EVUSNDQ04.sff', 'EVHINN115.sff', 'EVNG8PH02.sff', 'EVHINN114.sff',
+                                         'EVHINN111.sff', 'EVNG8PH03.sff', 'EVHINN109.sff']))
