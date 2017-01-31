9835feb88458c9a8617eef876598c9a93c982670
David Johnson <david.johnson@oerc.ox.ac.uk>
2017-01-31 16:36:53 +0000

    Implement #182

--- isatools/__main__.py
+++ isatools/__main__.py
@@ -1,6 +1,7 @@
 import argparse
 import sys
 import os
+import json
 
 
 def main(argv=None):
@@ -17,7 +18,7 @@ def main(argv=None):
                                 )
 
     p.add_argument('-c', dest='cmd', help='isatools API command to run', required=True,
-                   choices=['isatab2json', 'json2isatab'])
+                   choices=['isatab2json', 'json2isatab', 'isatab2sra', 'json2sra', 'zipisatab'])
     p.add_argument('-i', dest='in_path', help='in folder (files will be read from here)', required=True)
     p.add_argument('-o', dest='out_path', help='out folder (a new directory will be created here)', required=True)
     p.add_argument('--version', action='version', version='isatools {}'.format("0.6"))
@@ -29,6 +30,25 @@ def main(argv=None):
         print("{} input path: {}".format(os.linesep, args.in_path))
         print("output path: {}".format(args.out_path))
 
+    if args.cmd == 'isatab2json':
+        from isatools.convert import isatab2json
+        J = isatab2json.convert(args.in_path)
+        with open(args.out_path, 'w') as out_fp:
+            json.dump(J, out_fp)
+
+    elif args.cmd == 'json2isatab':
+        from isatools.convert import json2isatab
+        with open(args.in_path) as in_fp:
+            json2isatab.convert(in_fp, args.out_path)
+
+    elif args.cmd == 'isatab2sra':
+        raise NotImplementedError  # TODO: Finish this
+
+    elif args.cmd == 'json2sra':
+        raise NotImplementedError  # TODO: Finish this
+
+    elif args.cmd == 'zipisatab':
+        raise NotImplementedError  # TODO: Finish this
 
 if __name__ == '__main__':
     main()
