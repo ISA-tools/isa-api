9cb607cf580b1d71fbc5c7f409990bba94c5a7ed
David Johnson <david.johnson@oerc.ox.ac.uk>
2017-03-06 12:51:21 +0000

    Test on isatab.load() to investigate #200

--- tests/test_isatab.py
+++ tests/test_isatab.py
@@ -244,6 +244,14 @@ class TestIsaTabLoad(unittest.TestCase):
     def tearDown(self):
         shutil.rmtree(self._tmp_dir)
 
+    def test_isatab_load_issue200(self):
+        with open(os.path.join(self._tab_data_dir, 'issue200', 'i_Investigation.txt')) as fp:
+            ISA = isatab.load(fp)
+            self.assertEqual(len(ISA.studies[0].assays[0].materials['samples']), 7)
+            self.assertEqual(len(ISA.studies[0].assays[0].materials['other_material']), 7)
+            self.assertEqual(len(ISA.studies[0].assays[0].data_files), 2)
+            self.assertEqual(len(ISA.studies[0].assays[0].process_sequence), 11)
+
     def test_isatab_load_sdata201414_isa1(self):
         with open(os.path.join(self._tab_data_dir, 'sdata201414-isa1', 'i_Investigation.txt')) as fp:
             ISA = isatab.load(fp)
