1b071492e2b14a3868d61ea080885110072c3594
David Johnson <david.johnson@oerc.ox.ac.uk>
2017-03-14 22:47:08 +0000

    Comment out test using SciData ISA-Tab as not in ISAdatasets repo

--- tests/test_sampletab.py
+++ tests/test_sampletab.py
@@ -21,10 +21,10 @@ class UnitSampleTabLoad(unittest.TestCase):
             self.assertEqual(len(ISA.studies[0].materials['samples']), 1)
             print(isatab.dumps(ISA))
 
-    def test_sampletab_load_gsb_718(self):
-        with open(os.path.join(self._sampletab_data_dir, 'GSB-718.txt')) as fp:
-            ISA = sampletab.load(fp)
-            self.assertEqual(len(ISA.studies), 1)
-            self.assertEqual(len(ISA.studies[0].materials['sources']), 51)
-            self.assertEqual(len(ISA.studies[0].materials['samples']), 2409)
-            print(isatab.dumps(ISA))
+    # def test_sampletab_load_gsb_718(self):
+    #     with open(os.path.join(self._sampletab_data_dir, 'GSB-718.txt')) as fp:
+    #         ISA = sampletab.load(fp)
+    #         self.assertEqual(len(ISA.studies), 1)
+    #         self.assertEqual(len(ISA.studies[0].materials['sources']), 51)
+    #         self.assertEqual(len(ISA.studies[0].materials['samples']), 2409)
+    #         print(isatab.dumps(ISA))
