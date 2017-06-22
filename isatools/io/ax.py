4b8bb35c6a7eae0e85850f054ff7386c4ab304e8
David Johnson <david.johnson@oerc.ox.ac.uk>
2017-06-22 14:17:39 +0100

    Add function to grab MAGE-TAB and use magetab2isatab to convert to ISA-Tab #220

--- isatools/io/ax.py
+++ isatools/io/ax.py
@@ -2,6 +2,8 @@ import ftplib
 import logging
 import os
 import tempfile
+import shutil
+from isatools.convert import magetab2isatab
 
 EBI_FTP_SERVER = 'ftp.ebi.ac.uk'
 AX_EXPERIMENT_BASE_DIR = '/pub/databases/arrayexpress/data/experiment/'
@@ -12,14 +14,15 @@ logger = logging.getLogger(__name__)
 
 def get(arrayexpress_id, target_dir=None):
     """
-    This function downloads ISA content from the ArrayExpress FTP site.
+    This function downloads MAGE-TAB content from the ArrayExpress FTP site.
 
-    :param ax_experiment_id: Study identifier for ArrayExpress study to get, as a str (e.g. E-GEOD-59671)
-    :param target_dir: Path to write files to. If None, writes to temporary directory (generated on the fly)
+    :param ax_experiment_id: Experiment identifier for ArrayExpress study to get, as a str (e.g. E-GEOD-59671)
+    :param target_dir: Path to write MAGE-TAB files to. If None, writes to temporary directory (generated on the fly)
     :return: Path where the files were written to
 
     Example usage:
-        AX.get_study('E-GEOD-59671', '/tmp/ax')
+        from isatools.io import ax as AX
+        AX.get('E-GEOD-59671', '/tmp/ax')
     """
 
     idbits = arrayexpress_id.split('-')
@@ -56,3 +59,30 @@ def get(arrayexpress_id, target_dir=None):
             return target_dir
     else:
         raise ConnectionError("There was a problem connecting to ArrayExpress: " + response)
+
+
+def get_isatab(arrayexpress_id, target_dir=None):
+    """
+    This function downloads MAGE-TAB content as ISA-Tab from the ArrayExpress FTP site.
+
+    :param ax_experiment_id: Experiment identifier for ArrayExpress study to get, as a str (e.g. E-GEOD-59671)
+    :param target_dir: Path to write ISA-Tab files to. If None, writes to temporary directory (generated on the fly)
+    :return: Path where the files were written to
+
+    Example usage:
+        from isatools.io import ax as AX
+        AX.get_isatab('E-GEOD-59671', '/tmp/ax')
+    """
+    tmp_dir = tempfile.mkdtemp()
+    try:
+        get(arrayexpress_id=arrayexpress_id, target_dir=tmp_dir)
+        if target_dir is None:
+            target_dir = tempfile.mkdtemp()
+        logging.info("Using directory '{}'".format(target_dir))
+        with open(os.path.join(tmp_dir, "{}.idf.txt".format(arrayexpress_id))) as idf_fp:
+            magetab2isatab.convert(source_idf_fp=idf_fp, output_path=target_dir)
+    except Exception as e:
+        logger.fatal("Something went wrong: {}".format(e))
+    finally:
+        shutil.rmtree(tmp_dir)
+        return target_dir
