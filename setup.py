bb777f7995063410506faa814cad6eee9359a680
David Johnson <david.johnson@oerc.ox.ac.uk>
2017-06-23 10:17:03 +0100

    Update version number for release; update known issues in docs

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='isatools',
-    version='0.8.1',
+    version='0.8.2',
     packages=['isatools', 'isatools.convert', 'isatools.io', 'isatools.model'],
     package_data={'isatools': ['schemas/cedar/*.json',
                                'schemas/isa_model_version_1_0_schemas/core/*.json',
