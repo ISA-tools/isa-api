6a9069a2a223d765dcdd4b51f67b47040e99791f
David Johnson <david.johnson@oerc.ox.ac.uk>
2017-07-03 14:14:49 +0100

    Changes in isatab.load() means test against #201 will never catch failure, so comment it out

--- tests/test_isatab.py
+++ tests/test_isatab.py
@@ -333,13 +333,13 @@ class TestIsaTabLoad(unittest.TestCase):
             self.assertEqual(len(ISA.studies[0].assays[0].data_files), 2)
             self.assertEqual(len(ISA.studies[0].assays[0].process_sequence), 11)
 
-    def test_isatab_load_issue201(self):
-        with open(os.path.join(self._tab_data_dir, 'sdata201411-isa1', 'i_Investigation.txt')) as fp:
-            try:
-                isatab.load(fp, skip_load_tables=True)
-            except Exception as ex:
-                self.assertIsInstance(ex, IOError)
-                self.assertIn("There was a problem parsing the investigation section:", str(ex))
+    # def test_isatab_load_issue201(self):  # issue now not relevant due to changes in parser
+    #     with open(os.path.join(self._tab_data_dir, 'sdata201411-isa1-parsererror', 'i_Investigation.txt')) as fp:
+    #         try:
+    #             self.assertRaises(isatab.load(fp, skip_load_tables=True), IOError)
+    #         except Exception as ex:  # This now doesn't fail as section loader is now more resilient
+    #             self.assertIsInstance(ex, IOError)
+    #             self.assertIn("There was a problem parsing the investigation section:", str(ex))
 
     def test_isatab_load_sdata201414_isa1(self):
         with open(os.path.join(self._tab_data_dir, 'sdata201414-isa1', 'i_Investigation.txt')) as fp:
