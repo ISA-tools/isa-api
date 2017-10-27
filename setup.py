0b7b846b12191d5f8a6189106a057d0406bb6673
David Johnson <david.johnson@oerc.ox.ac.uk>
2017-10-27 16:39:22 +0100

    Update release to add test tag

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='isatools',
-    version='0.9.3-lite',
+    version='0.9.3-lite_test',
     packages=['isatools',
               'isatools.errors'
               ],
