6a359108ab00d1bd39b65719b1f21b7a08fcab17
Philippe Rocca-Serra <philippe.rocca-serra@oerc.ox.ac.uk>
2017-11-06 17:47:05 +0000

    adding qc_terms configuration file, MTBLS ISA file cleaners

---
+++ isatools/create/MTBLS-purge-spurious-factors.py
@@ -0,0 +1,25 @@
+from isatools.utils import IsaTabFixer
+import os
+import re
+
+
+fixer = IsaTabFixer('/Users/Philippe/Downloads/ftp.ebi.ac.uk/pub/databases/metabolights/studies/public/MTBLS81/s_Study id.txt')
+
+this_factor="Age at sacrifice"
+
+fixer.fix_factor(this_factor)  # fixes by moving factor to charac
+
+# spurious_factors = "factor: Age at sacrifice"
+#
+# factornames = []
+# factornames = spurious_factors.split("factor: ")
+#
+# for element in factornames:
+#
+#     this_factor = element.strip()
+#     this_factor = re.sub(";","", this_factor)
+#     this_factor = this_factor.strip()
+#     print(this_factor)
+
+
+
