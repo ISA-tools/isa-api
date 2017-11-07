7891fcf727ad5fc08d99af6f4292561010b41842
David Johnson <david.johnson@oerc.ox.ac.uk>
2017-11-07 11:54:34 +0000

    Try fix bash script to get test data

--- get_test_data.sh
+++ get_test_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $TRAVIS_BRANCH == 'develop' ]]
+if [ $TRAVIS_BRANCH == 'develop' ]; then
   git clone -b tests-dev --single-branch http://github.com/ISA-tools/ISAdatasets tests/data
 else
   git clone -b tests --single-branch http://github.com/ISA-tools/ISAdatasets tests/data
