5388a293c028916cd746a0eb92b0ce4f8026eb35
David Johnson <david.johnson@oerc.ox.ac.uk>
2017-11-07 12:05:06 +0000

    Fixes build error due to no main function in new mtbls fixer scripts

--- isatools/create/MTBLS-purge-spurious-factors.py
+++ isatools/create/MTBLS-purge-spurious-factors.py
@@ -3,23 +3,25 @@ import os
 import re
 
 
-fixer = IsaTabFixer('/Users/Philippe/Downloads/ftp.ebi.ac.uk/pub/databases/metabolights/studies/public/MTBLS81/s_Study id.txt')
+def main():
+    fixer = IsaTabFixer('/Users/Philippe/Downloads/ftp.ebi.ac.uk/pub/databases/metabolights/studies/public/MTBLS81/s_Study id.txt')
 
-this_factor="Age at sacrifice"
+    this_factor="Age at sacrifice"
 
-fixer.fix_factor(this_factor)  # fixes by moving factor to charac
+    fixer.fix_factor(this_factor)  # fixes by moving factor to charac
 
-# spurious_factors = "factor: Age at sacrifice"
-#
-# factornames = []
-# factornames = spurious_factors.split("factor: ")
-#
-# for element in factornames:
-#
-#     this_factor = element.strip()
-#     this_factor = re.sub(";","", this_factor)
-#     this_factor = this_factor.strip()
-#     print(this_factor)
+    # spurious_factors = "factor: Age at sacrifice"
+    #
+    # factornames = []
+    # factornames = spurious_factors.split("factor: ")
+    #
+    # for element in factornames:
+    #
+    #     this_factor = element.strip()
+    #     this_factor = re.sub(";","", this_factor)
+    #     this_factor = this_factor.strip()
+    #     print(this_factor)
 
 
-
+if __name__ == '__main__':
+    main()
