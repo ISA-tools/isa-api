89558c5241f0ed70985c94ed88e2f30c2bd65a27
djcomlab <djcomlab@gmail.com>
2018-06-21 14:33:26 +0100

    Fix incorrect tests

--- tests/test_create_models_study_design.py
+++ tests/test_create_models_study_design.py
@@ -795,10 +795,10 @@ class IsaModelObjectFactoryTest(unittest.TestCase):
         self.investigation.studies = [study]
         # 36 sources, 56 QC sources
         self.assertEqual(86, len(study.sources))
-        self.assertEqual(37, len(
+        self.assertEqual(38, len(
             [x for x in study.sources
              if x.get_char('Material Type').value.term == 'solvent']))
-        self.assertEqual(13, len(
+        self.assertEqual(12, len(
             [x for x in study.sources
              if x.get_char('Material Type').value.term == 'blank']))
         # 288 samples plus 36 QC samples
