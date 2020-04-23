25640958aeb000f453e0e8fed3bd94233a502e7f
zigur <massimorgon@gmail.com>
2020-04-23 19:08:49 +0100

    implemented support for ontology annotation (with test) #355

--- isatools/create/connectors.py
+++ isatools/create/connectors.py
@@ -1,13 +1,65 @@
+from isatools.model import OntologyAnnotation, OntologySource
 from isatools.create.models import AssayGraph
 from collections import OrderedDict
+from numbers import Number
+
+
+def _map_ontology_annotations(annotation):
+    if isinstance(annotation, dict):
+        res = OntologyAnnotation(
+            term=annotation['term'],
+            term_accession=annotation.get('iri', None)
+        )
+        source = annotation.get('source', None)
+        if source:
+            if isinstance(source, str):
+                res.term_source = OntologySource(name=source)
+            elif isinstance(source, dict):
+                res.term_source = OntologySource(**source)
+        return res
+    else:
+        return annotation
+
+
+def _reverse_map_ontology_annotation(onto_annotation):
+    if isinstance(onto_annotation, OntologyAnnotation):
+        res = dict(term=onto_annotation.term, iri=onto_annotation.term_accession)
+        if onto_annotation.term_source:
+            if isinstance(onto_annotation.term_source, str):
+                res['source'] = onto_annotation.term_source
+            elif isinstance(onto_annotation.term_source, OntologySource):
+                res['source'] = dict(
+                    name=onto_annotation.term_source.name,
+                    file=onto_annotation.term_source.file,
+                    version=onto_annotation.term_source.version,
+                    description=onto_annotation.term_source.description
+                )
+        return res
+    else:
+        return onto_annotation
 
 
 def assay_template_convert_json_to_ordered_dict(assay_template_json):
     res = OrderedDict()
     res['measurement_type'] = assay_template_json['measurement_type']
     res['technology_type'] = assay_template_json['technology_type']
-    for name, item in assay_template_json['workflow']:
-        res[name] = item
+    for name, nodes in assay_template_json['workflow']:
+        prepared_nodes = None
+        if isinstance(nodes, list):
+            prepared_nodes = [
+                {key: _map_ontology_annotations(value) for key, value in el.items()} for el in nodes
+            ]
+        if isinstance(nodes, dict):
+            prepared_nodes = {}
+            for param_name, param_values in nodes.items():
+                # if it is a special key (e.g."#replicates") leave it alone
+                if param_name[0] == '#' and not isinstance(param_values, list):
+                    prepared_nodes[param_name] = param_values
+                else:
+                    prepared_nodes[param_name] = [
+                        _map_ontology_annotations(param_value) for param_value in param_values
+                    ]
+        res[_map_ontology_annotations(name)] = prepared_nodes
     return res
 
 
--- tests/test_create_connectors.py
+++ tests/test_create_connectors.py
@@ -11,6 +11,7 @@ from tests.test_create_models_study_design import ms_assay_dict
 class TestMappings(unittest.TestCase):
 
     def setUp(self):
+        self.maxDiff = None
         file_path = os.path.abspath(
             os.path.join(
                 os.path.dirname(__file__), 'data', 'json', 'create', 'templates', 'metabolite-profiling-ms.json'
@@ -21,8 +22,8 @@ class TestMappings(unittest.TestCase):
 
     def test_assay_template_convert_json_to_ordered_dict_met_prof_mass_spec(self):
         actual_odict_mp_ms = assay_template_convert_json_to_ordered_dict(self.json_mp_ms)
-        self.assertEquals(actual_odict_mp_ms, ms_assay_dict)
+        self.assertEqual(actual_odict_mp_ms, ms_assay_dict)
 
     def test_assay_template_convert_ordered_dict_to_json_met_prof_mass_spec(self):
         actual_json_mp_ms = assay_template_convert_ordered_dict_to_json(ms_assay_dict)
-        self.assertEquals(actual_json_mp_ms, self.json_mp_ms)
+        self.assertEqual(actual_json_mp_ms, self.json_mp_ms)
