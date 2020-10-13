ed87dc8f89599af95280b663b5083c888a4b3ce6
djcomlab <djcomlab@gmail.com>
2020-10-13 22:14:23 +0200

    Clean up sampletab2isatab tests

--- tests/test_sampletab2isatab.py
+++ tests/test_sampletab2isatab.py
@@ -32,17 +32,10 @@ class TestSampleTab2IsaTab(unittest.TestCase):
         with open(os.path.join(self._sampletab_dir, "test2.txt")) as sampletab_fp:
             sampletab2isatab.convert(source_sampletab_fp=sampletab_fp, target_dir=self._tmp_dir)
 
-    # FIXME this test should be fixed before merging the PR
-    @unittest.skip("skipping test to see if Travis works")
-    def test_sampletab2json_GSB_3(self):
-        with open(os.path.join(self._sampletab_dir, "GSB-3.txt")) as sampletab_fp:
-            sampletab2isatab.convert(source_sampletab_fp=sampletab_fp, target_dir=self._tmp_dir)
-
     def test_sampletab2json_GSB_537(self):
         with open(os.path.join(self._sampletab_dir, "GSB-537.txt")) as sampletab_fp:
             sampletab2isatab.convert(source_sampletab_fp=sampletab_fp, target_dir=self._tmp_dir)
 
-    @unittest.skip("skipping test that hangs on like forever")
     def test_sampletab2json_GSB_718(self):
         with open(os.path.join(self._sampletab_dir, "GSB-718.txt")) as sampletab_fp:
             sampletab2isatab.convert(source_sampletab_fp=sampletab_fp, target_dir=self._tmp_dir)
