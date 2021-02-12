68303c2235fd977d7a95f0f06043896d045b210b
zigur <massimorgon@gmail.com>
2021-02-12 17:56:11 +0000

    added format to long_documentation

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
     description='Metadata tracking tools help to manage an increasingly diverse set of life science, '
                 'environmental and biomedical experiments',
     long_description=read('README.md'),
+    long_description_content_type='text/markdown',
     author='ISA Infrastructure Team',
     author_email='isatools@googlegroups.com',
     url='https://github.com/ISA-tools/isa-api',
