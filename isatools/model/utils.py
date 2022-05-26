b143f842de5e9668ae23c64691724fb0b28143b2
Terazus <batistadominique@hotmail.com>
2022-05-26 07:38:44 -0700

    added utils tests

--- isatools/model/utils.py
+++ isatools/model/utils.py
@@ -2,6 +2,9 @@ import networkx as nx
 
 from isatools.model.datafile import DataFile
 from isatools.model.process import Process
+from isatools.model.source import Source
+from isatools.model.sample import Sample
+from isatools.model.material import Material
 
 
 def find(predictor, iterable):
@@ -56,3 +59,156 @@ def plink(p1, p2):
     if isinstance(p1, Process) and isinstance(p2, Process):
         p1.next_process = p2
         p2.prev_process = p1
+
+
+def batch_create_materials(material=None, n=1):
+    """Creates a batch of material objects (Source, Sample or Material) from a
+    prototype material object
+
+    :param material: existing material object to use as a prototype
+    :param n: Number of material objects to create in the batch
+    :returns: List of material objects
+
+    :Example:
+
+        # Create 10 sample materials derived from one source material
+
+        source = Source(name='source_material')
+        prototype_sample = Sample(name='sample_material', derives_from=[source])
+        batch = batch_create_materials(prototype_sample, n=10)
+
+        [Sample<>, Sample<>, Sample<>, Sample<>, Sample<>, Sample<>, Sample<>,
+        Sample<>, Sample<>, Sample<>, ]
+
+    """
+    material_list = list()
+    if isinstance(material, (Source, Sample, Material)):
+        for x in range(0, n):
+            new_obj = _deep_copy(material)
+            new_obj.name = material.name + '-' + str(x)
+            if hasattr(material, 'derives_from'):
+                new_obj.derives_from = material.derives_from
+
+            material_list.append(new_obj)
+
+    return material_list
+
+
+def batch_create_assays(*args, n=1):
+    """Creates a batch of assay process sequences (Material->Process->Material)
+    from a prototype sequence (currently works only as flat end-to-end
+    processes of Material->Process->Material->...)
+
+    :param *args: An argument list representing the process sequence prototype
+    :param n: Number of process sequences to create in the batch
+    :returns: List of process sequences replicating the prototype sequence
+
+    :Example:
+
+        # Create 3 assays of (Sample -> Process -> Material -> Process ->
+        LabeledExtract)
+
+        sample = Sample(name='sample', derives_from=[Source(name='source')])
+        data_acquisition = Process(name='data acquisition')
+        material = Material(name='material')
+        labeling = Process(name='labeling')
+        extract = LabeledExtract(name='lextract')
+        batch = batch_create_assays(sample, data_acquisition, material,
+        labeling, extract, n=3)
+
+        [Process<> Process<>, Process<> Process<>, Process<>, Process<>]
+
+        # Create 3 assays of ([Sample, Sample] -> Process -> [Material,
+        Material])
+
+        sample1 = Sample(name='sample')
+        sample2 = Sample(name='sample')
+        process = Process(name='data acquisition')
+        material1 = Material(name='material')
+        material2 = Material(name='material')
+        batch = batch_create_assays([sample1, sample2], process, [material1,
+        material2], n=3)
+
+    """
+
+    process_sequence = []
+    material_a = None
+    process = None
+    material_b = None
+    for x in range(0, n):
+        for arg in args:
+            if isinstance(arg, list) and len(arg) > 0:
+                if isinstance(arg[0], (Source, Sample, Material)):
+                    if material_a is None:
+                        material_a = _deep_copy(arg)
+                        y = 0
+                        for material in material_a:
+                            material.name = material.name + '-' + str(x) + '-' + str(y)
+                            y += 1
+                    else:
+                        material_b = _deep_copy(arg)
+                        y = 0
+                        for material in material_b:
+                            material.name = material.name + '-' + str(x) + '-' + str(y)
+                            y += 1
+                elif isinstance(arg[0], Process):
+                    process = _deep_copy(arg)
+                    y = 0
+                    for p in process:
+                        p.name = p.name + '-' + str(x) + '-' + str(y)
+                        y += 1
+            if isinstance(arg, (Source, Sample, Material)):
+                if material_a is None:
+                    material_a = _deep_copy(arg)
+                    material_a.name = material_a.name + '-' + str(x)
+                else:
+                    material_b = _deep_copy(arg)
+                    material_b.name = material_b.name + '-' + str(x)
+            elif isinstance(arg, Process):
+                process = _deep_copy(arg)
+                process.name = process.name + '-' + str(x)
+            if material_a is not None and material_b is not None and process is not None:
+                if isinstance(process, list):
+                    for p in process:
+                        if isinstance(material_a, list):
+                            p.inputs = material_a
+                        else:
+                            p.inputs.append(material_a)
+                        if isinstance(material_b, list):
+                            p.outputs = material_b
+                            for material in material_b:
+                                material.derives_from = [material_a]
+                        else:
+                            p.outputs.append(material_b)
+                            material_b.derives_from = [material_a]
+                        process_sequence.append(process)
+                else:
+                    if isinstance(material_a, list):
+                        process.inputs = material_a
+                    else:
+                        process.inputs.append(material_a)
+                    if isinstance(material_b, list):
+                        process.outputs = material_b
+                        for material in material_b:
+                            material.derives_from = [material_a]
+                    else:
+                        process.outputs.append(material_b)
+                        material_b.derives_from = [material_a]
+                    process_sequence.append(process)
+                material_a = material_b
+                process = None
+                material_b = None
+    return process_sequence
+
+
+def _deep_copy(isa_object):
+    """
+    Re-implementation of the deepcopy function that also increases and sets the object identifiers for copied objects.
+    :param {Object} isa_object: the object to copy
+    """
+    from copy import deepcopy
+    from isatools.model.process_sequence import ProcessSequenceNode
+    new_obj = deepcopy(isa_object)
+    if isinstance(isa_object, ProcessSequenceNode):
+        new_obj.assign_identifier()
+    return new_obj
--- tests/model/test_utils.py
+++ tests/model/test_utils.py
@@ -2,9 +2,16 @@ from unittest import TestCase
 
 from isatools.model.datafile import DataFile
 from isatools.model.sample import Sample
-from isatools.model.material import Material
+from isatools.model.material import Material, Extract, LabeledExtract
 from isatools.model.process import Process
-from isatools.model.utils import _build_assay_graph, find, plink
+from isatools.model.source import Source
+from isatools.model.utils import (
+    _build_assay_graph,
+    find, plink,
+    batch_create_materials,
+    batch_create_assays,
+    _deep_copy
+)
 
 
 class TestUtils(TestCase):
@@ -35,3 +42,64 @@ class TestUtils(TestCase):
         plink(first_process, second_process)
         self.assertEqual(first_process.next_process, second_process)
         self.assertEqual(second_process.prev_process, first_process)
+
+    def test__deep_copy(self):
+        process = Process()
+        self.assertTrue(process == _deep_copy(process))
+
+        extract = Extract()
+        self.assertTrue(extract == _deep_copy(extract))
+
+        material = Material()
+        self.assertNotEqual(material, _deep_copy(material))
+
+    def test_batch_create_materials(self):
+        source = Source(name='source_material')
+        prototype_sample = Sample(name='sample_material', derives_from=[source])
+        batch = batch_create_materials(prototype_sample, n=2)
+        for sample in batch:
+            self.assertTrue(isinstance(sample, Sample))
+            self.assertEqual(sample.derives_from, [source])
+
+    def test_batch_create_assays(self):
+        sample = Sample(name='sample', derives_from=[Source(name='source')])
+        data_acquisition = Process(name='data acquisition')
+        material = Material(name='material')
+        process = Process(name='process name')
+        labeled_extract = LabeledExtract(name='lextract')
+        batch = batch_create_assays(sample, data_acquisition, material, process, labeled_extract, n=2)
+        self.assertEqual(len(batch), 4)
+        self.assertEqual(batch[0].name, 'data acquisition-0')
+        self.assertEqual(batch[1].name, 'process name-0')
+        self.assertEqual(batch[2].name, 'data acquisition-1')
+        self.assertEqual(batch[3].name, 'process name-1')
+        for item in batch:
+            self.assertIsInstance(item, Process)
+
+        sample1 = Sample(name='sample')
+        sample2 = Sample(name='sample')
+        process = Process(name='data acquisition')
+        material1 = Material(name='material')
+        material2 = Material(name='material')
+        batch = batch_create_assays([sample1, sample2], process, [material1, material2], n=2)
+        self.assertEqual(batch[0].name, 'data acquisition-0')
+        self.assertEqual(batch[1].name, 'data acquisition-1')
+        self.assertEqual(len(batch), 2)
+        for item in batch:
+            self.assertIsInstance(item, Process)
+
+        process = Process(name='data acquisition')
+        batch = batch_create_assays([process], [material1, material2], n=2)
+        self.assertEqual(len(batch), 1)
+
+        source = Source()
+        first_batch = batch_create_assays([sample1], [process], [source], n=2)
+        second_batch = batch_create_assays([process], [sample1], [source], n=2)
+        third_batch = batch_create_assays([process], [source], [sample1], n=2)
+        self.assertEqual(first_batch, second_batch)
+        self.assertFalse(first_batch == third_batch)
+
+        first_batch = batch_create_assays(sample1, [process], source, n=2)
+        self.assertFalse(first_batch == third_batch)
+        self.assertFalse(first_batch == second_batch)
+
