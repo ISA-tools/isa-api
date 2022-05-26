5a4bee486bd7a7dbd3327a54663e6f14a6a48b98
Terazus <batistadominique@hotmail.com>
2022-05-26 07:50:21 -0700

    Added protocol component test

--- isatools/model/__init__.py
+++ isatools/model/__init__.py
@@ -12,8 +12,6 @@ Todo:
 .. _ISA Model and Serialization Specs 1.0: http://isa-specs.readthedocs.io/
 
 """
-from __future__ import absolute_import
-
 from isatools.model.assay import Assay
 from isatools.model.characteristic import Characteristic
 from isatools.model.comments import Commentable, Comment
@@ -44,6 +42,7 @@ from isatools.model.person import Person
 from isatools.model.process import Process
 from isatools.model.process_sequence import ProcessSequenceNode
 from isatools.model.protocol import Protocol, load_protocol_types_info
+from isatools.model.protocol_component import ProtocolComponent
 from isatools.model.protocol_parameter import ProtocolParameter
 from isatools.model.publication import Publication
 from isatools.model.sample import Sample
@@ -51,83 +50,6 @@ from isatools.model.source import Source
 from isatools.model.study import Study
 from isatools.model.logger import log
 from isatools.model.utils import _build_assay_graph, plink, batch_create_assays, batch_create_materials, _deep_copy
-
-
-class ProtocolComponent(Commentable):
-    """A component used in a protocol.
-
-    Attributes:
-        name: A component name.
-        component_type: The classifier as a term for the component.
-        comments: Comments associated with instances of this class.
-    """
-
-    def __init__(self, id_='', name='', component_type=None, comments=None):
-        super().__init__(comments)
-
-        self.id = id_
-        self.__name = name
-
-        if component_type is None:
-            self.__component_type = OntologyAnnotation()
-        else:
-            self.__component_type = component_type
-
-    @property
-    def name(self):
-        """:obj:`str`: the name of the protocol component"""
-        return self.__name
-
-    @name.setter
-    def name(self, val):
-        if val is not None and not isinstance(val, str):
-            raise AttributeError(
-                'ProtocolComponent.name must be a str or None; got {0}:{1}'
-                    .format(val, type(val)))
-        else:
-            self.__name = val
-
-    @property
-    def component_type(self):
-        """ :obj:`OntologyAnnotation`: a component_type for the protocol
-        component"""
-        return self.__component_type
-
-    @component_type.setter
-    def component_type(self, val):
-        if val is not None and not isinstance(val, OntologyAnnotation):
-            raise AttributeError(
-                'ProtocolComponent.component_type must be a '
-                'OntologyAnnotation, or None; got {0}:{1}'.format(
-                    val, type(val)))
-        else:
-            self.__component_type = val
-
-    def __repr__(self):
-        return "isatools.model.ProtocolComponent(name='{component.name}', " \
-               "category={component_type}, " \
-               "comments={component.comments})".format(
-            component=self, component_type=repr(self.component_type))
-
-    def __str__(self):
-        return """ProtocolComponent(
-    name={component.name}
-    category={component_type}
-    comments={num_comments} Comment objects
-)""".format(component=self, component_type=self.component_type.term if
-        self.component_type else '', num_comments=len(self.comments))
-
-    def __hash__(self):
-        return hash(repr(self))
-
-    def __eq__(self, other):
-        return isinstance(other, ProtocolComponent) \
-               and self.name == other.name \
-               and self.component_type == other.component_type \
-               and self.comments == other.comments
-
-    def __ne__(self, other):
-        return not self == other
 
 
 class ISADocument:
@@ -157,3 +79,4 @@ class ISADocument:
     @property
     def valid_isajson(self):
         return True
+
---
+++ isatools/model/protocol_component.py
@@ -0,0 +1,76 @@
+from isatools.model.comments import Commentable
+from isatools.model.ontology_annotation import OntologyAnnotation
+
+
+class ProtocolComponent(Commentable):
+    """A component used in a protocol.
+
+    Attributes:
+        name: A component name.
+        component_type: The classifier as a term for the component.
+        comments: Comments associated with instances of this class.
+    """
+
+    def __init__(self, id_='', name='', component_type=None, comments=None):
+        super().__init__(comments)
+
+        self.id = id_
+        self.__name = name
+
+        if component_type is None:
+            self.__component_type = OntologyAnnotation()
+        else:
+            self.__component_type = component_type
+
+    @property
+    def name(self):
+        """:obj:`str`: the name of the protocol component"""
+        return self.__name
+
+    @name.setter
+    def name(self, val):
+        if val is not None and not isinstance(val, str):
+            raise AttributeError('ProtocolComponent.name must be a str or None; got {0}:{1}'.format(val, type(val)))
+        self.__name = val
+
+    @property
+    def component_type(self):
+        """ :obj:`OntologyAnnotation`: a component_type for the protocol
+        component"""
+        return self.__component_type
+
+    @component_type.setter
+    def component_type(self, val):
+        if val is not None and not isinstance(val, OntologyAnnotation):
+            raise AttributeError(
+                'ProtocolComponent.component_type must be a '
+                'OntologyAnnotation, or None; got {0}:{1}'.format(
+                    val, type(val)))
+        else:
+            self.__component_type = val
+
+    def __repr__(self):
+        return "isatools.model.ProtocolComponent(name='{component.name}', " \
+               "category={component_type}, " \
+               "comments={component.comments})".format(
+            component=self, component_type=repr(self.component_type))
+
+    def __str__(self):
+        return """ProtocolComponent(
+    name={component.name}
+    category={component_type}
+    comments={num_comments} Comment objects
+)""".format(component=self, component_type=self.component_type.term if
+        self.component_type else '', num_comments=len(self.comments))
+
+    def __hash__(self):
+        return hash(repr(self))
+
+    def __eq__(self, other):
+        return isinstance(other, ProtocolComponent) \
+               and self.name == other.name \
+               and self.component_type == other.component_type \
+               and self.comments == other.comments
+
+    def __ne__(self, other):
+        return not self == other
