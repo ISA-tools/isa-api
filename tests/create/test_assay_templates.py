89db7ea0399fdd29300ba4a11beabe8e32a1634d
Philippe Rocca-Serra <proccaserra@gmail.com>
2022-11-18 14:05:42 +0000

    minor fix on tests

--- tests/create/test_assay_templates.py
+++ tests/create/test_assay_templates.py
@@ -4,43 +4,7 @@ import isatools
 from isatools.create import errors
 from isatools.model import (
     OntologyAnnotation,
-    StudyFactor,
-    FactorValue,
-    Characteristic,
-    Sample,
-    ProtocolParameter,
-    ParameterValue,
-    Study,
-    Assay,
-    Process
-)
-from isatools.create.model import (
-    NonTreatment,
-    Treatment,
-    TreatmentFactory,
-    StudyCell,
-    ProductNode,
-    ProtocolNode,
-    SequenceNode,
-    AssayGraph,
-    SampleAndAssayPlan,
-    StudyArm,
-    StudyDesign,
-    StudyDesignFactory,
-    QualityControl,
-    QualityControlSample,
-    QualityControlService
-)
-from isatools.create.constants import (
-    SCREEN, RUN_IN, WASHOUT, FOLLOW_UP, ELEMENT_TYPES, INTERVENTIONS, DURATION_FACTOR,
-    BASE_FACTORS_, BASE_FACTORS, SOURCE, SAMPLE, EXTRACT, LABELED_EXTRACT, default_ontology_source_reference,
-    DEFAULT_SOURCE_TYPE, QC_SAMPLE_TYPE_PRE_RUN, QC_SAMPLE_TYPE_INTERSPERSED, DEFAULT_STUDY_IDENTIFIER
-)
-from isatools.tests.create_sample_assay_plan_odicts import (
-    sample_list,
-    ms_assay_dict,
-    lcdad_assay_dict,
-    nmr_assay_dict
+    OntologySource
 )
 
 from isatools.create.assay_templates import create_new_ontology_annotation
@@ -50,9 +14,9 @@ class MyTestCase(unittest.TestCase):
 
     def setUp(self):
         self.processed_ontology_annotation = {}
+        self.ontosrc = OntologySource(name='toto')
 
     def test_create_new_ontology_annotation(self, term_name="something"):
         stuff = isatools.create.assay_templates.create_new_ontology_annotation(term_name),
-        self.assertEqual(str(stuff), str((isatools.model.OntologyAnnotation(term='something', term_source=None, term_accession='', comments=[]),)))
-
-
+        self.assertEqual(str(stuff), str((isatools.model.OntologyAnnotation(term='something', term_source=self.ontosrc,
+                                                                            term_accession='', comments=[]),)))
