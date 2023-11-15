818332d5f194414a546512cea2697b0b9e5843da
Philippe Rocca-Serra <proccaserra@gmail.com>
2023-11-15 16:09:04 +0000

    adding a test to check if comments on assay declaration are properly serialized in isatab2json conversion

--- tests/convert/test_isatab2json.py
+++ tests/convert/test_isatab2json.py
@@ -4,6 +4,7 @@ from isatools.convert import isatab2json
 import json
 from isatools.tests import utils
 from isatools import isajson
+from isatools import isatab
 import tempfile
 import shutil
 
@@ -113,3 +114,28 @@ class TestIsaTab2Json(unittest.TestCase):
         with open(os.path.join(self._tmp_dir, 'isa.json')) as actual_json:
             report = isajson.validate(actual_json)
             self.assertEqual(len(report['errors']), 0)
+
+    def test_isatab2json_convert_comment(self):
+        with open(os.path.join(self._tab_data_dir, 'issue200', 'i_Investigation.txt')) as fp:
+            ISA = isatab.load(fp)
+            self.assertEqual(ISA.studies[0].assays[0].comments[0].value, "ena")
+            self.assertEqual(ISA.ontology_source_references[0].comments[0].name, "onto_comment")
+            self.assertEqual(ISA.ontology_source_references[0].comments[0].value, "onto_stuff")
+            self.assertEqual(ISA.studies[0].protocols[0].comments[0].value, "another protocol related comment")
+            self.assertEqual(ISA.studies[0].protocols[2].comments[0].value, "protocol related comment")
+            self.assertEqual(ISA.studies[0].protocols[3].comments[0].value, "")
+            self.assertEqual(ISA.studies[0].contacts[0].comments[0].name, "person comment")
+            self.assertEqual(ISA.studies[0].factors[0].comments[0].value, "stf_cmt")
+
+        test_case = "issue200"
+        actual_json = isatab2json.convert(
+                os.path.join(self._tab_data_dir, test_case), validate_first=False,
+                use_new_parser=True)
+        with open(os.path.join(self._tmp_dir, 'isa.json'), 'w') as out_fp:
+            json.dump(actual_json, out_fp)
+
+        with open(os.path.join(self._tmp_dir, 'isa.json')) as isa_json:
+            isajson_read = json.load(isa_json)
+            self.assertEqual(isajson_read["studies"][0]["filename"], "s_Study id.txt")
+            self.assertEqual(isajson_read["studies"][0]["assays"][0]["comments"][0]["value"], "ena")
+
