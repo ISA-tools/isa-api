b1d770c9473a2efbb695dd358df40af35447cd93
Philippe Rocca-Serra <proccaserra@gmail.com>
2024-02-14 09:38:15 +0000

    linting

--- isatools/convert/json2sra.py
+++ isatools/convert/json2sra.py
@@ -13,26 +13,24 @@ def convert(json_fp, path, config_dir=None, sra_settings=None,
     """ Converter for ISA-JSON to SRA.
     :param json_fp: File pointer to ISA JSON input
     :param path: Directory for output SRA XMLs to be written
-    :param config_dir: path to JSON configuration. If none, uses default
-        embedded in API
+    :param config_dir: path to JSON configuration. If none, uses default embedded in API
     :param sra_settings: SRA settings dict
     :param datafilehashes: Data files with hashes, in a dict
     :param validate_first: a boolean flag to indicate whether to validate or not before converting
     """
     if validate_first:
         log.info("Validating input JSON before conversion")
-        report = isajson.validate(fp=json_fp, config_dir=config_dir,
+        report = isajson.validate(fp=json_fp,
+                                  config_dir=config_dir,
                                   log_level=logging.ERROR)
         if len(report.get('errors')) > 0:
-            log.fatal("Could not proceed with conversion as there are some "
-                      "validation errors. Check log.")
+            log.fatal("Could not proceed with conversion as there are some validation errors. Check log.")
             return
     log.info("Loading isajson {}".format(json_fp.name))
     isa = isajson.load(fp=json_fp)
     log.info("Exporting SRA to {}".format(path))
     log.debug("Using SRA settings ".format(sra_settings))
-    sra.export(isa, path, sra_settings=sra_settings,
-               datafilehashes=datafilehashes)
+    sra.export(isa, path, sra_settings=sra_settings, datafilehashes=datafilehashes)
 
 
 """
