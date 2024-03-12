1a8dab1722e63ba96ec7544a745e030281a76c40
Travis Thompson <ptth222@uky.edu>
2024-03-12 14:37:31 -0400

    Update test_core.py
    The DOI fix reduced 2 more warnings. Somehow that fix wasn't in this branch after rebasing.

--- tests/isatab/validate/test_core.py
+++ tests/isatab/validate/test_core.py
@@ -17,7 +17,7 @@ class TestValidators(unittest.TestCase):
         data_path = path.join(path.dirname(path.abspath(__file__)), '..', '..', 'data', 'tab', 'BII-S-3')
         with open(path.join(data_path, 'i_gilbert.txt'), 'r') as data_file:
             r = validate(fp=data_file, config_dir=self.default_conf, origin="")
-        self.assertEqual(len(r['warnings']), 4)
+        self.assertEqual(len(r['warnings']), 2)
 
     def test_mtbls267(self):
         data_path = path.join(path.dirname(path.abspath(__file__)), '..', '..', 'data', 'tab', 'MTBLS267-partial')
@@ -82,7 +82,7 @@ class TestValidators(unittest.TestCase):
         data_path = path.join(path.dirname(path.abspath(__file__)), '..', '..', 'data', 'tab', 'BII-S-3')
         with open(path.join(data_path, 'i_gilbert.txt'), 'r') as data_file:
             r = validate(data_file, rules=rules)
-        self.assertEqual(len(r['warnings']), 4)
+        self.assertEqual(len(r['warnings']), 2)
 
         rule = '12000'
         expected_error = {
